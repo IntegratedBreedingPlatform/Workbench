1c81a26c1bc6e898ae44791e8daf4ff994595b53
alex <alex@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-11-12 04:07:31 +0000

    refactor: implemented filtering of location by country,by type and country and type
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWorkbench@1627 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/ibpworkbench/comp/project/create/ProjectLocationsComponent.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/project/create/ProjectLocationsComponent.java
@@ -21,7 +21,9 @@ import org.generationcp.ibpworkbench.actions.OpenAddLocationWindowAction;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.ManagerFactory;
 import org.generationcp.middleware.manager.api.ManagerFactoryProvider;
+import org.generationcp.middleware.pojos.Country;
 import org.generationcp.middleware.pojos.Location;
+import org.generationcp.middleware.pojos.UserDefinedField;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.generationcp.middleware.pojos.workbench.Project;
 import org.slf4j.Logger;
@@ -36,7 +38,10 @@ import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Select;
 import com.vaadin.ui.TwinColSelect;
 import com.vaadin.ui.VerticalLayout;
 
@@ -59,12 +64,17 @@ public class ProjectLocationsComponent extends VerticalLayout implements Initial
     private Button showLocationWindowButton;
     private Component buttonArea;
     BeanItemContainer<Location> beanItemContainer;
-    TwinColSelect select;
 
 	@Autowired
     private ManagerFactoryProvider managerFactoryProvider;
-	private Button testButton;
-
+	private VerticalLayout locationLayout;
+	private GridLayout gridLocationLayout;
+	private Select selectLocationCountry;
+	private Select selectLocationType;
+	private Button btnFilterLocation;
+	private TwinColSelect selectLocation;
+	private ManagerFactory managerFactory;
+	private CropType cropType;
 	
     public ProjectLocationsComponent(CreateProjectPanel createProjectPanel) {
         this.createProjectPanel = createProjectPanel;
@@ -79,48 +89,25 @@ public class ProjectLocationsComponent extends VerticalLayout implements Initial
         assemble();
     }
 
-    protected void assemble() {
+    protected void assemble() throws MiddlewareQueryException {
         initializeComponents();
-        initializeValues();
+    	initializeValues();
         initializeLayout();
         initializeActions();
     }
 
-    protected void initializeComponents() {
+    protected void initializeComponents() throws MiddlewareQueryException {
         setSpacing(true);
         setMargin(true);
 
-        select = new TwinColSelect();
-        select.setLeftColumnCaption("Available Locations");
-        select.setRightColumnCaption("Selected Locations");
-        select.setRows(10);
-        select.setWidth("690px");
-        select.setMultiSelect(true);
-        select.setNullSelectionAllowed(true);
-
-        CropType cropType = createProjectPanel.getSelectedCropType();
-        if (cropType != null) {
-            try {
-                Container container = createLocationsContainer(cropType);
-                select.setContainerDataSource(container);
-
-                for (Object itemId : container.getItemIds()) {
-                    Location location = (Location) itemId;
-                    select.setItemCaption(itemId, location.getLname());
-                }
-            } catch (MiddlewareQueryException e) {
-                LOG.error("Error encountered while getting central locations", e);
-                throw new InternationalizableException(e, Message.DATABASE_ERROR, Message.CONTACT_ADMIN_ERROR_DESC);
-            }
-        }
-
-        addComponent(select);
-
+        addComponent(layoutLocationArea());
         buttonArea = layoutButtonArea();
         addComponent(buttonArea);
+        
     }
 
-    protected void initializeValues() {
+    protected void initializeValues() throws MiddlewareQueryException {
+      	  
     }
 
     protected void initializeLayout() {
@@ -139,29 +126,150 @@ public class ProjectLocationsComponent extends VerticalLayout implements Initial
         buttonLayout.setSpacing(true);
         buttonLayout.setMargin(true, false, false, false);
 
-        showLocationWindowButton = new Button("Add Location");
+        showLocationWindowButton = new Button("Add New Location");
         previousButton = new Button("Previous");
 
         buttonLayout.addComponent(showLocationWindowButton);
         buttonLayout.addComponent(previousButton);
         return buttonLayout;
     }
+    
+    private Component layoutLocationArea() throws MiddlewareQueryException {
 
-    private Container createLocationsContainer(CropType cropType) throws MiddlewareQueryException {
-        ManagerFactory managerFactory = managerFactoryProvider.getManagerFactoryForCropType(cropType);
+        locationLayout = new VerticalLayout();
+        gridLocationLayout = new GridLayout();
+        gridLocationLayout.setRows(3);
+        gridLocationLayout.setColumns(4);
+        gridLocationLayout.setSpacing(true);
 
-        beanItemContainer = new BeanItemContainer<Location>(Location.class);
+        selectLocationCountry = new Select();
+    	selectLocationCountry.addItem("");
+        populateCountryList();
+        selectLocationCountry.select("1");
+        selectLocationCountry.setNullSelectionAllowed(false);
+
+        selectLocationType = new Select();
+        selectLocationType.addItem("");
+        populateLocationTypeList();
+        selectLocationType.select("");
+        selectLocationType.setNullSelectionAllowed(false);
+
+        btnFilterLocation = new Button("Filter");
+        btnFilterLocation.addListener(new ClickListener() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+            	selectLocation.removeAllItems();
+                cropType = createProjectPanel.getSelectedCropType();
+                if (cropType != null) {
+                    try {
+                        Container container = createLocationsContainer(cropType);
+                        selectLocation.setContainerDataSource(container);
+
+                        for (Object itemId : container.getItemIds()) {
+                            Location loc = (Location) itemId;
+                            selectLocation.setItemCaption(itemId, loc.getLname());
+                        }
+                    } catch (MiddlewareQueryException e) {
+                        LOG.error("Error encountered while getting central methods", e);
+                        throw new InternationalizableException(e, Message.DATABASE_ERROR, Message.CONTACT_ADMIN_ERROR_DESC);
+                    }
+                }
+            }
+        });
+
+        gridLocationLayout.addComponent(new Label("Select Country To Filter"), 1, 1);
+        gridLocationLayout.addComponent(selectLocationCountry, 2, 1);
+        gridLocationLayout.addComponent(new Label("Select Location Type To Filter"), 1, 2);
+        gridLocationLayout.addComponent(selectLocationType, 2, 2);
+        gridLocationLayout.addComponent(btnFilterLocation, 3, 1);
+
+        selectLocation = new TwinColSelect();
+        selectLocation.setLeftColumnCaption("Available Locations");
+        selectLocation.setRightColumnCaption("Selected Locations");
+        selectLocation.setRows(10);
+        selectLocation.setWidth("690px");
+        selectLocation.setMultiSelect(true);
+        selectLocation.setNullSelectionAllowed(true);
+
+        if (cropType != null) {
+        	selectLocation.removeAllItems();
+            try {
+                Container container = createLocationsContainer(cropType);
+                selectLocation.setContainerDataSource(container);
+
+                for (Object itemId : container.getItemIds()) {
+                    Location location = (Location) itemId;
+                    selectLocation.setItemCaption(itemId, location.getLname());
+                }
+            } catch (MiddlewareQueryException e) {
+                LOG.error("Error encountered while getting central locations", e);
+                throw new InternationalizableException(e, Message.DATABASE_ERROR, Message.CONTACT_ADMIN_ERROR_DESC);
+            }
+        }
+
+        locationLayout.addComponent(gridLocationLayout);
+        locationLayout.addComponent(selectLocation);
+
+        return locationLayout;
+
+    }
+
+	private void populateCountryList() throws MiddlewareQueryException {
+    	cropType = createProjectPanel.getSelectedCropType();
+    	ManagerFactory managerFactory=managerFactoryProvider.getManagerFactoryForCropType(cropType);
+    	List<Country> countryList=managerFactory.getGermplasmDataManager().getAllCountry();
+    	 
+    	 for(Country c: countryList){
+    	       selectLocationCountry.addItem(String.valueOf(c.getCntryid()));
+    	       selectLocationCountry.setItemCaption(String.valueOf(c.getCntryid()), c.getIsoabbr());
+    	}
+		
+	}
+	
+    private void populateLocationTypeList() throws MiddlewareQueryException {
+    	
+    	ManagerFactory managerFactory=managerFactoryProvider.getManagerFactoryForCropType(cropType);
+    	List<UserDefinedField> userDefineField=managerFactory.getGermplasmDataManager().getUserDefinedFieldByFieldTableNameAndType("LOCATION","LTYPE");
+    	 
+    	 for(UserDefinedField u: userDefineField){
+    		 selectLocationType.addItem(String.valueOf(u.getFldno()));
+    		 selectLocationType.setItemCaption(String.valueOf(u.getFldno()), u.getFname());
+    	}
+	}
+
+	private Container createLocationsContainer(CropType cropType) throws MiddlewareQueryException {
+		
+		ManagerFactory managerFactory = managerFactoryProvider.getManagerFactoryForCropType(cropType);
+		beanItemContainer = new BeanItemContainer<Location>(Location.class);
         if (managerFactory == null) {
             return beanItemContainer;
         }
+        String locationCountryID =selectLocationCountry.getValue().toString();
+        String locationType = selectLocationType.getValue().toString();
+        Country country;
 
         long locCount = managerFactory.getGermplasmDataManager().countAllLocations();
-        List<Location> locationList = managerFactory.getGermplasmDataManager().getAllLocations(0, (int) locCount);
-
-        for (Location location : locationList) {
-            beanItemContainer.addBean(location);
+        List<Location> locationList = null;
+        if (selectLocationCountry.getValue().toString() != "" && selectLocationType.getValue().toString() == "") {
+        	country=managerFactory.getGermplasmDataManager().getCountryById(Integer.valueOf(locationCountryID));
+        	locCount=(int) managerFactory.getGermplasmDataManager().countLocationsByCountry(country);
+        	locationList = managerFactory.getGermplasmDataManager().getLocationsByCountry(country, 0, (int)locCount);
+        } else if (selectLocationCountry.getValue().toString() == "" && selectLocationType.getValue().toString() != "") {
+        	locationList = managerFactory.getGermplasmDataManager().getLocationsByType(Integer.valueOf(locationType));
+        } else if (selectLocationCountry.getValue().toString() != "" && selectLocationType.getValue().toString() != "") {
+        	country=managerFactory.getGermplasmDataManager().getCountryById(Integer.valueOf(locationCountryID));
+        	locationList = managerFactory.getGermplasmDataManager().getLocationsByCountryAndType(country, Integer.valueOf(locationType));
+        } else {
+//        	locationList = managerFactory.getGermplasmDataManager().getAllLocations(0, (int) locCount);
         }
 
+        for (Location loc : locationList) {
+            beanItemContainer.addBean(loc);
+        }
+        
         return beanItemContainer;
     }
 
@@ -169,11 +277,10 @@ public class ProjectLocationsComponent extends VerticalLayout implements Initial
     public boolean validate(){
         return true;
     }
-    
 
     public boolean validateAndSave(){
         if (validate()){ // save if valid
-            Set<Location> locations = (Set<Location>) select.getValue();
+            Set<Location> locations = (Set<Location>) selectLocation.getValue();
             Project project = createProjectPanel.getProject();
             project.setLocations(locations);
             createProjectPanel.setProject(project);
@@ -195,7 +302,7 @@ public class ProjectLocationsComponent extends VerticalLayout implements Initial
 	}
 
 	public TwinColSelect getSelect() {
-		return select;
+		return selectLocation;
 	}
 
 
