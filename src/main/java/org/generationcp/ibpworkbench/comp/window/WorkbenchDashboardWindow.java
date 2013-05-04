a0d919c84d1154bbfa98c344e19de98239010f02
glenn <glenn@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-04 08:34:29 +0000

    updated workbench version
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWorkbench@2897 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/ibpworkbench/comp/window/WorkbenchDashboardWindow.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/window/WorkbenchDashboardWindow.java
@@ -54,7 +54,7 @@ import com.vaadin.ui.themes.BaseTheme;
 public class WorkbenchDashboardWindow extends Window implements IContentWindow, InitializingBean, InternationalizableComponent {
     private static final long serialVersionUID = 1L;
     
-    private static final String VERSION = "1.1.3.11";
+    private static final String VERSION = "1.1.4.1";
     
     private Label workbenchTitle;
     private Button homeButton;
