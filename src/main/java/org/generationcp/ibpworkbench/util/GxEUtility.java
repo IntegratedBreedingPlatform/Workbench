387ed27641fa3d6ffea7b6688bf12aae4f54eb3c
aldrin <aldrin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-09 02:04:25 +0000

    GCP-3881
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWorkbench@2994 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/ibpworkbench/util/GxEUtility.java
@@ -0,0 +1,87 @@
+package org.generationcp.ibpworkbench.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import com.vaadin.data.Property;
+import com.vaadin.ui.*;
+
+public class GxEUtility {
+	
+	public static Table generateTestData(Table table, Integer numRows) throws Exception{
+		
+		ArrayList<Class<?>> arr =  new ArrayList<Class<?>>();
+		Object item = table.addItem();
+		for (Iterator i = table.getContainerPropertyIds().iterator(); i.hasNext();) {
+			Property p = table.getContainerProperty(item, i.next());
+			arr.add(p.getType());
+		}
+		table.removeItem(item);
+		for (Integer c = 1;c <= numRows; c++ ){
+			
+			Object[] obj = new Object[arr.size()];
+			for(Integer c2 = 0; c2 < arr.size(); c2++){
+				obj[c2] = createObjectCaption(arr.get(c2), "Data" + c, c2);
+			}
+		
+			System.out.println(table.addItem(obj, new Integer(c)));
+					
+		}
+		
+		return table;
+	
+	}
+	
+	public static Object createObjectCaption(Class<?> propertyType, String value, Integer colIndex) throws Exception{
+	
+		if (propertyType.equals(CheckBox.class) || propertyType.isInstance(CheckBox.class)){
+			CheckBox o = new CheckBox();
+			if (colIndex > 1){
+				o.setCaption(randomInRange(1,100).toString());
+			}else{
+				o.setCaption(value);
+			}
+			
+			return o;
+		}else if (propertyType.equals(Label.class)){
+			Label o = new Label();
+			o.setCaption(value);
+			o.setValue(value);
+			return o;
+		}else if (propertyType.equals(Link.class)){
+			Link o = new Link();
+			o.setCaption(value);
+			return o;
+		}else if (propertyType.equals(Link.class)){
+			Link o = new Link();
+			o.setCaption(value);
+			return o;
+		}else if (propertyType.equals(TextField.class)){
+			TextField o = new TextField();
+			o.setCaption(value);
+			o.setValue(value);
+			return o;
+		}else if (propertyType.equals(String.class)){
+			return value;
+		}else if (propertyType.equals(Integer.class)){
+			return new Random().nextInt(100);
+		}else if (propertyType.equals(Double.class)){
+			return randomInRange(1,100);
+		}else{
+			throw new Exception(String.format("Property Type: {%s} is not yet supported.", propertyType.toString()));
+		}
+	
+	
+	}
+	
+	protected static Random random = new Random();
+	
+	public static Double randomInRange(double min, double max) {
+	  double range = max - min;
+	  double scaled = random.nextDouble() * range;
+	  double shifted = scaled + min;
+	  return shifted; 
+	
+	}
+}
