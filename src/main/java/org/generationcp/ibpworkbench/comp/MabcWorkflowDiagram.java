8bbfa62253f64a2507f0d4109a8c2b9a967133c8
billy <billy@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-07 14:36:50 +0000

    GCP-4434
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWorkbench@3869 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/ibpworkbench/comp/MabcWorkflowDiagram.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/MabcWorkflowDiagram.java
@@ -44,7 +44,7 @@ public class MabcWorkflowDiagram extends VerticalLayout implements WorkflowConst
 
     //this is in pixels and used for layouting
     private static final int WORKFLOW_STEP_HEIGHT = 125;
-    private static final int WORKFLOW_STEP_EXTRA_HEIGHT = 155;
+    private static final int WORKFLOW_STEP_EXTRA_HEIGHT = 200;
     private static final int WORKFLOW_STEP_WIDTH = 270;
     private static final int EXTRA_SPACE_BETWEEN_COMPONENTS = 10;
     private static final int ARROW_IMAGE_HEIGHT = 30;
