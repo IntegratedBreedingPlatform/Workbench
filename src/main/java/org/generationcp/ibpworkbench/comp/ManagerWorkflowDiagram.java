0e2f090fb8773552f38ca0fa5578f7d31486effe
cyrus <cyrus@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-08-19 04:38:07 +0000

    GCP-1005 Fix layout issues on the manager workflow
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWorkbench@4262 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/ibpworkbench/comp/ManagerWorkflowDiagram.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/ManagerWorkflowDiagram.java
@@ -47,7 +47,7 @@ public class ManagerWorkflowDiagram extends VerticalLayout implements WorkflowCo
     private static final long serialVersionUID = 1L;
 
     //this is in pixels and used for layouting
-    private static final int WORKFLOW_STEP_HEIGHT = 180;
+    private static final int WORKFLOW_STEP_HEIGHT = 200;
     private static final int WORKFLOW_STEP_WIDTH = 270;
     private static final int EXTRA_SPACE_BETWEEN_COMPONENTS = 10;
     
@@ -300,7 +300,7 @@ public class ManagerWorkflowDiagram extends VerticalLayout implements WorkflowCo
         
         layout.setMargin(true);
         layout.setWidth("850px");
-        layout.setHeight("420px");
+        layout.setHeight("440px");
         
         String extraSpace = EXTRA_SPACE_BETWEEN_COMPONENTS + "px";
         int top = 10;
@@ -537,7 +537,7 @@ public class ManagerWorkflowDiagram extends VerticalLayout implements WorkflowCo
         
         Label emptyLabel = new Label(" ");
         emptyLabel.setWidth("100%");
-        emptyLabel.setHeight("68px");
+        emptyLabel.setHeight("90px");
         layout.addComponent(emptyLabel);
         layout.setExpandRatio(emptyLabel, 100);
 
