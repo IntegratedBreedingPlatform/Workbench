88167bf5cf213f9311c0e901a4274b6947098f3b
Aldrin Batac <aldrin@efficio.us.com>
2013-09-17 11:07:05 +0800

    [10:59:31 AM] Rachel Lucas: 1.) REmove Breeding Planner from CB workflow; Project Planning step [11:00:36 AM] Rachel Lucas: 2.) REmove Breeding Planner from MAS workflow; Project Planning step

--- src/main/java/org/generationcp/ibpworkbench/comp/ConventionalBreedingWorkflowDiagram.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/ConventionalBreedingWorkflowDiagram.java
@@ -308,10 +308,12 @@ public class ConventionalBreedingWorkflowDiagram extends VerticalLayout implemen
         layout.addComponent(emptyLabel);
         layout.setExpandRatio(emptyLabel, 100);
         
+        /**
         layout.addComponent(breedingPlannerButton);
         breedingPlannerButton.setHeight("20px");
         layout.setComponentAlignment(breedingPlannerButton, Alignment.TOP_CENTER);
         layout.setExpandRatio(breedingPlannerButton, 0);
+        **/
         
         layout.addComponent(browseGermplasmButton);
         browseGermplasmButton.setHeight("20px");
