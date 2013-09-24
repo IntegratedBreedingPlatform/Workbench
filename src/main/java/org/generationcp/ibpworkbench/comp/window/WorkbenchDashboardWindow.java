fbf999a9f362cfb8ca550ea8279458b1dcd66bea
Glenn Marintes <gmarintes@gmail.com>
2013-09-24 21:14:58 +0800

    updated workbench version

--- src/main/java/org/generationcp/ibpworkbench/comp/window/WorkbenchDashboardWindow.java
+++ src/main/java/org/generationcp/ibpworkbench/comp/window/WorkbenchDashboardWindow.java
@@ -58,7 +58,7 @@ import com.vaadin.ui.themes.Reindeer;
 public class WorkbenchDashboardWindow extends Window implements IContentWindow, InitializingBean, InternationalizableComponent {
     private static final long serialVersionUID = 1L;
     
-    public static final String VERSION = "1.1.4.21";
+    public static final String VERSION = "1.1.4.26";
     public static final String HELP_LINK = "https://www.integratedbreeding.net/manuals-and-tutorials-ib-tools";
     private Label workbenchTitle;
     private Button homeButton;
