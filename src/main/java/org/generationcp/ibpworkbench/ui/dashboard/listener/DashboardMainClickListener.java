0151afeb2b8926fbb66eaf73f754b09bfc1ec618
Cyrus Venn Casada <cvpcasada@outlook.com>
2013-11-20 15:33:04 +0800

    Added dashboard click listener

---
+++ src/main/java/org/generationcp/ibpworkbench/ui/dashboard/listener/DashboardMainClickListener.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2013, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package org.generationcp.ibpworkbench.ui.dashboard.listener;
+
+import org.generationcp.commons.exceptions.InternationalizableException;
+import org.generationcp.commons.vaadin.util.MessageNotifier;
+import org.generationcp.ibpworkbench.ui.dashboard.WorkbenchDashboard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+
+
+/**
+ * @author Efficio.Daniel
+ *
+ */
+public class DashboardMainClickListener implements ClickListener{
+
+    private Long projectId;
+    private Component source;
+    
+    private static final Logger LOG = LoggerFactory.getLogger(DashboardMainClickListener.class);
+    
+    public DashboardMainClickListener(Component source, Long projectId){
+        this.projectId = projectId;
+        this.source = source;
+    }
+    /* (non-Javadoc)
+     * @see com.vaadin.ui.Button.ClickListener#buttonClick(com.vaadin.ui.Button.ClickEvent)
+     */
+    @Override
+    public void buttonClick(ClickEvent event) {
+        // TODO Auto-generated method stub
+        if (event.getButton().getData().equals(WorkbenchDashboard.BUTTON_LIST_MANAGER_COLUMN_ID)
+                && (source instanceof WorkbenchDashboard)){
+            try {
+                System.out.println("Open list manager" + this.projectId);
+            } catch (InternationalizableException e){
+                LOG.error(e.toString() + "\n" + e.getStackTrace());
+                e.printStackTrace();
+                MessageNotifier.showError(event.getComponent().getWindow(), e.getCaption(), e.getDescription());
+            }
+        } 
+    }
+    
+
+}
