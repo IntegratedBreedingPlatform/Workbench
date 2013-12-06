2529f9b70d2c044a7dfd73c3cbd1dc2b95292c2f
Aldrin Batac <aldrin@efficio.us.com>
2013-12-06 14:08:59 +0800

    GCP-6397

---
+++ src/main/java/org/generationcp/ibpworkbench/ui/metaanalysis/SelectTraitsForMetaAnalysisPanel.java
@@ -0,0 +1,29 @@
+package org.generationcp.ibpworkbench.ui.metaanalysis;
+
+import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.vaadin.ui.VerticalLayout;
+
+@Configurable
+public class SelectTraitsForMetaAnalysisPanel extends VerticalLayout implements InitializingBean, InternationalizableComponent {
+
+
+	public SelectTraitsForMetaAnalysisPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateLabels() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
