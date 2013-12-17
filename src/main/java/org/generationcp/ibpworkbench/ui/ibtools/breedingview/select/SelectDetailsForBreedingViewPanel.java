c19f2e5e5c181a5adcf1d72ebe9599ae89b4073a
Aldrin Batac <aldrin@efficio.us.com>
2013-12-17 15:28:56 +0800

    GCP-6827

--- src/main/java/org/generationcp/ibpworkbench/ui/ibtools/breedingview/select/SelectDetailsForBreedingViewPanel.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/ibtools/breedingview/select/SelectDetailsForBreedingViewPanel.java
@@ -443,6 +443,7 @@ public class SelectDetailsForBreedingViewPanel extends VerticalLayout implements
         
         selDesignType = new Select();
         selDesignType.setImmediate(true); 
+        selDesignType.setNullSelectionAllowed(true);
         selDesignType.addItem(DesignType.INCOMPLETE_BLOCK_DESIGN.getName());
         selDesignType.addItem(DesignType.RANDOMIZED_BLOCK_DESIGN.getName());
         selDesignType.addItem(DesignType.ROW_COLUMN_DESIGN.getName());
@@ -800,7 +801,11 @@ public class SelectDetailsForBreedingViewPanel extends VerticalLayout implements
     }
     
     private void reset(){
-    	
+    	selEnvFactor.select((Object) null);
+    	selDesignType.select((Object) null);
+    	footerCheckBox.setValue(false);
+    	txtAnalysisName.setValue(getBreedingViewInput().getBreedingViewAnalysisName());
+    
     }
 
     protected void initializeActions() {
