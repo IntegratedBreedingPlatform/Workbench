99dc218cc20918becedb3c7e10b29c490c08cf0b
Glenn Marintes <glenn@efficio.us.com>
2014-01-17 07:26:38 -0600

    updated version to 2.0.3

--- src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
@@ -53,7 +53,7 @@ import com.vaadin.ui.themes.Reindeer;
 public class WorkbenchMainView extends Window implements IContentWindow, InitializingBean, InternationalizableComponent {
     private static final long serialVersionUID = 1L;
 
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.0.3";
     public static final String HELP_LINK = "https://www.integratedbreeding.net/manuals-and-tutorials-ib-tools";
     private Label workbenchTitle;
     private Button homeButton;
