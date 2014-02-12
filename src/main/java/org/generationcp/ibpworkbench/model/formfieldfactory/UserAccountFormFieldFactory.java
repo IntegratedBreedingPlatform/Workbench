1fd1299c2b176fcdf7e6e22d3185cbba28f82d29
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-02-12 17:13:25 +0800

    added periods in  validation message

--- src/main/java/org/generationcp/ibpworkbench/model/formfieldfactory/UserAccountFormFieldFactory.java
+++ src/main/java/org/generationcp/ibpworkbench/model/formfieldfactory/UserAccountFormFieldFactory.java
@@ -102,12 +102,12 @@ public class UserAccountFormFieldFactory extends DefaultFieldFactory{
         if ("firstName".equals(propertyId)) {
             messageSource.setCaption(firstName, Message.USER_ACC_FNAME);
             firstName.setRequired(true);
-            lastName.setRequiredError("Please enter First Name");
+            lastName.setRequiredError("Please enter First Name.");
             return firstName;
         } else if ("lastName".equals(propertyId)) {
             messageSource.setCaption(lastName, Message.USER_ACC_LNAME);
             lastName.setRequired(true);
-            lastName.setRequiredError("Please enter Last Name");
+            lastName.setRequiredError("Please enter Last Name.");
             return lastName;
         } else if ("middleName".equals(propertyId)) {
             messageSource.setCaption(field, Message.USER_ACC_MIDNAME);
