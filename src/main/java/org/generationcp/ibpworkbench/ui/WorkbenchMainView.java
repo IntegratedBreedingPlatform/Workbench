3cbfaf71e79140cd93b30221a64bee6ceb627f05
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-02-24 18:28:02 +0800

    UI improvements on header font, increased size of sidebar toggle (more hit space)

--- src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
@@ -197,7 +197,7 @@ public class WorkbenchMainView extends Window implements IContentWindow, Initial
         collapseButton = new Button("<span class='glyphicon icon-menu'></span>");
         collapseButton.setStyleName(Bootstrap.Buttons.LINK.styleName() + " collapse-btn");
         collapseButton.setHtmlContentAllowed(true);
-        collapseButton.setDescription("Collapse Sidebar");
+        collapseButton.setDescription(messageSource.getMessage("TOGGLE_SIDEBAR"));
 
         crumbTrail = new CrumbTrail();
         //crumbTrail.setMargin(true, true, true, true);
