1fca4cfe790f9b51724875f81e5f9c5817574586
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-10-02 13:45:21 +0800

    Remove unused import files

--- src/main/java/org/generationcp/ibpworkbench/ui/window/SecurityQuestionWindow.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/window/SecurityQuestionWindow.java
@@ -12,10 +12,10 @@
 
 package org.generationcp.ibpworkbench.ui.window;
 
-import com.vaadin.ui.Window;
-import com.vaadin.ui.themes.Reindeer;
 import org.generationcp.commons.vaadin.ui.BaseSubWindow;
 import org.generationcp.ibpworkbench.ui.ForgotPasswordUsernamePanel;
+
+import com.vaadin.ui.themes.Reindeer;
 
 public class SecurityQuestionWindow extends BaseSubWindow {
     
