011d56afc1602be67a1d0c5c943e98ec4c820142
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-10-08 12:15:44 +0800

    Added stub unit test class for GermplasmListPreview
    GCP-10159
    
    Reviewer: Mae Turiana

---
+++ src/test/java/org/generationcp/ibpworkbench/ui/dashboard/preview/GermplasmListPreviewPresenterTest.java
@@ -0,0 +1,62 @@
+package org.generationcp.ibpworkbench.ui.dashboard.preview;
+
+import junit.framework.TestCase;
+
+public class GermplasmListPreviewPresenterTest extends TestCase {
+
+    public void testGenerateInitialTreeNode() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testAddGermplasmListNode() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testHasChildList() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testGetManagerFactory() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testSetManagerFactory() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testIsFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testHasChildren() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testGetGermplasmListParent() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testRenameGermplasmListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testAddGermplasmListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testValidateForDeleteGermplasmList() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testDeleteGermplasmListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testDropGermplasmListToParent() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testUpdateProjectLastOpenedDate() throws Exception {
+        //TODO: ADD TEST
+    }
+}
