bf322f3a69e2ef475ea172e3b8db11a04bd1569b
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-10-08 12:11:09 +0800

    Added stub unit test calss for NurseryListPreveiwPresenter
    GCP-10159
    
    Reviewer: Mae Turiana

---
+++ src/test/java/org/generationcp/ibpworkbench/ui/dashboard/preview/NurseryListPreviewPresenterTest.java
@@ -0,0 +1,58 @@
+package org.generationcp.ibpworkbench.ui.dashboard.preview;
+
+import junit.framework.TestCase;
+
+public class NurseryListPreviewPresenterTest extends TestCase {
+
+    public void testGenerateInitialTreeNodes() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testGetManagerFactory() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testSetManagerFactory() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testIsFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testRenameNurseryListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testDeleteNurseryListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testGetStudyNodeParent() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testMoveNurseryListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testAddNurseryListFolder() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testValidateForDeleteNurseryList() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testAddChildrenNode() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testUpdateProjectLastOpenedDate() throws Exception {
+        //TODO: ADD TEST
+    }
+
+    public void testGetStudyType() throws Exception {
+        //TODO: ADD TEST
+    }
+}
