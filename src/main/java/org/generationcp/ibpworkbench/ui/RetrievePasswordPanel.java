8ec8d965568ae54959959f0bdad737faa7b565d5
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-10-09 16:13:03 +0800

    Renamed Ids to make it compliant to the naming convention vaadin-<name>-<type>
    GCP-10145
    
    Reviewer: Daniel Jao

--- src/main/java/org/generationcp/ibpworkbench/ui/RetrievePasswordPanel.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/RetrievePasswordPanel.java
@@ -61,7 +61,7 @@ public class RetrievePasswordPanel extends VerticalLayout implements Initializin
         Label securityQuestionLabel = new Label(securityQuestion.getSecurityQuestion());
         addComponent(securityQuestionLabel);
         securityAnswerField = new TextField();
-        securityAnswerField.setDebugId("vaadin-answer-forgotpassword");
+        securityAnswerField.setDebugId("vaadin-answer-txt");
         addComponent(securityAnswerField);
         
         retrieveBtn = new Button(messageSource.getMessage(Message.BTN_RETRIEVE));
