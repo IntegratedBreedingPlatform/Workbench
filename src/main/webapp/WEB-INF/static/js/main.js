76979a77fca6fa4ff94679a3868071c5bde60d9e
Helen Durrant <hh.durrant@gmail.com>
2014-11-17 21:53:54 +1300

    Committing unminified web files - better for dev.

--- src/main/webapp/WEB-INF/static/js/main.js
+++ src/main/webapp/WEB-INF/static/js/main.js
@@ -1,1 +1,50 @@
-!function(){"use strict";function e(){o.hasClass(s)?o.removeClass(s):o.addClass(s)}function n(){var e=!t.hasClass(c);t.toggleClass(c,e),l.text(e?r:i),a.text(e?i:r)}var o=$(".js-login-check"),t=$(".js-login-form"),l=$(".js-login-mode-toggle"),a=$(".js-login-submit"),c="create-account",s="fa fa-check",i="Create Account",r="Sign In";$(".js-login-checkbox-control").on("click",".js-login-remember-me",function(n){n.preventDefault(),e()}),l.on("click",function(e){e.preventDefault(),n()}),Modernizr.mq("only all")||$("html").addClass("no-mq"),$("input, textarea").placeholder()}();
+/*global Modernizr*/
+
+(function() {
+	'use strict';
+
+	var $check = $('.js-login-check'),
+		$loginForm = $('.js-login-form'),
+		$loginModeToggle = $('.js-login-mode-toggle'),
+		$loginSubmit = $('.js-login-submit'),
+
+		createAccount = 'create-account',
+		tick = 'fa fa-check',
+
+		createAccountText = 'Create Account',
+		signInText = 'Sign In';
+
+	function toggleCheckbox() {
+		if ($check.hasClass(tick)) {
+			$check.removeClass(tick);
+		} else {
+			$check.addClass(tick);
+		}
+	}
+
+	function toggleLoginCreateAccount() {
+		var loginDisplayed = !$loginForm.hasClass(createAccount);
+
+		$loginForm.toggleClass(createAccount, loginDisplayed);
+		$loginModeToggle.text(loginDisplayed ?  signInText : createAccountText);
+		$loginSubmit.text(loginDisplayed ? createAccountText : signInText);
+	}
+
+	$('.js-login-checkbox-control').on('click', '.js-login-remember-me', function(e) {
+		e.preventDefault();
+		toggleCheckbox();
+	});
+
+	$loginModeToggle.on('click', function(e) {
+		e.preventDefault();
+		toggleLoginCreateAccount();
+	});
+
+	// Record whether media queries are supported in this browser as a class
+	if (!Modernizr.mq('only all')) {
+		$('html').addClass('no-mq');
+	}
+
+	// Initialise placeholder polyfill plugin
+	$('input, textarea').placeholder();
+}());
