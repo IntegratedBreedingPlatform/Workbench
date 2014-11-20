d3f008bbbe03074984c52ece63c6b81c16b6b7e5
Lisa Quayle <lisa@leafnode.io>
2014-11-21 10:18:13 +1300

    Added validation to login form and prevent request being sent unless username and password are provided.

--- src/main/web/src/js/main.js
+++ src/main/web/src/js/main.js
@@ -7,9 +7,13 @@
 		$loginForm = $('.js-login-form'),
 		$loginModeToggle = $('.js-login-mode-toggle'),
 		$loginSubmit = $('.js-login-submit'),
+		$error = $('.js-login-error'),
+		$username = $('.js-login-username'),
+		$password = $('.js-login-password'),
 
 		createAccount = 'create-account',
 		tick = 'fa fa-check',
+		validationError = 'login-validation-error',
 
 		createAccountText = 'Create Account',
 		signInText = 'Sign In';
@@ -30,13 +34,59 @@
 		$loginSubmit.text(loginDisplayed ? createAccountText : signInText);
 	}
 
+	function clearServerErrors() {
+		$('.js-th-login-error').empty();
+	}
+
+	function clearClientErrors() {
+		$('.' + validationError).removeClass(validationError);
+		$error.empty();
+	}
+
+	function inputsValidate() {
+		var isUsernameEmpty = !$username.val(),
+			validates = !isUsernameEmpty,
+			errorMessage;
+
+		if (isUsernameEmpty) {
+			$username.addClass(validationError);
+			errorMessage = 'Please provide a username.';
+		}
+		if (!$password.val()) {
+			$password.addClass(validationError);
+			errorMessage = isUsernameEmpty ? 'Please provide a username and password.' : 'Please provide a password.';
+			validates = false;
+		}
+
+		$error.text(errorMessage);
+
+		return validates;
+	}
+
 	$('.js-login-checkbox-control').on('click', '.js-login-remember-me', function(e) {
 		e.preventDefault();
 		toggleCheckbox();
 	});
 
+	$('.js-login-form').on('submit', function(e) {
+		e.preventDefault();
+		clearServerErrors();
+
+		// Perform validation on input fields
+		if (inputsValidate()) {
+			// Continue with form submit
+			this.submit();
+		}
+	});
+
+	$('.js-login-form').on('focusin', '.login-validation-error', function() {
+		clearClientErrors();
+	});
+
 	$loginModeToggle.on('click', function(e) {
 		e.preventDefault();
+		clearClientErrors();
+		clearServerErrors();
 		toggleLoginCreateAccount();
 	});
 
