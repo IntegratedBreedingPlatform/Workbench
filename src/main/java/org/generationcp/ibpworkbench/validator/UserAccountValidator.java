fe254122417d59a77d2df444215f1c811b30cdf7
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-12-02 17:11:14 +0800

    Fixes 'Utility classes should not have a public constructor' violation from SonarQube
    BMS-119
    
    Reviewer: Crisjaira Villarino

--- src/main/java/org/generationcp/ibpworkbench/validator/UserAccountValidator.java
+++ src/main/java/org/generationcp/ibpworkbench/validator/UserAccountValidator.java
@@ -115,14 +115,4 @@ public class UserAccountValidator implements Validator {
 			LOG.error(e.getMessage(),e);
 		}
 	}
-
-	public class UserAccountFields {
-		public static final String FIRST_NAME = "firstName";
-		public static final String LAST_NAME = "lastName";
-		public static final String EMAIL = "email";
-		public static final String USERNAME = "username";
-		public static final String PASSWORD = "password";
-		public static final String PASSWORD_CONFIRMATION = "passwordConfirmation";
-		public static final String ROLE = "role";
-	}
 }
