f9bf5baaa260daa07fcede5ab626d393e493a7df
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-12-11 11:07:32 +0800

    Added new validations for field length, email for acount creation
    BMS-119
    
    Reviewer: Daniel Villafuerte

--- src/test/java/org/generationcp/ibpworkbench/validator/UserAccountValidatorTest.java
+++ src/test/java/org/generationcp/ibpworkbench/validator/UserAccountValidatorTest.java
@@ -4,6 +4,7 @@ import org.generationcp.commons.security.Role;
 import org.generationcp.ibpworkbench.model.UserAccountModel;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import org.generationcp.middleware.pojos.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -11,6 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.validation.Errors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -54,13 +58,19 @@ public class UserAccountValidatorTest {
 
 		partialValidator.validate(userAccount, errors);
 
+		verify(partialValidator, times(3))
+				.validateFieldLength(any(Errors.class), anyString(), anyString(), anyString(),
+						anyInt());
+
+		verify(partialValidator).validateEmailFormat(errors, userAccount);
+
 		verify(partialValidator).validateFieldsEmptyOrWhitespace(errors);
 
 		verify(partialValidator).validatePasswordConfirmationIfEquals(errors, userAccount);
 
 		verify(partialValidator).validateUsernameIfExists(errors, userAccount);
 
-		verify(partialValidator).validatePersonIfExists(errors, userAccount);
+		verify(partialValidator).validatePersonEmailIfExists(errors, userAccount);
 
 		verify(partialValidator).validateUserRole(errors, userAccount);
 
@@ -158,71 +168,6 @@ public class UserAccountValidatorTest {
 	}
 
 	@Test
-	public void testValidatePersonIfExists() throws Exception {
-		ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
-		ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
-
-		UserAccountModel userAccount = new UserAccountModel();
-		userAccount.setFirstName("firstName");
-		userAccount.setLastName("lastName");
-
-		when(workbenchDataManager
-				.isPersonExists(userAccount.getFirstName(), userAccount.getLastName()))
-				.thenReturn(true);
-
-		UserAccountValidator partialValidator = spy(validator);
-		partialValidator.validatePersonIfExists(errors, userAccount);
-
-		verify(errors).rejectValue(arg1.capture(), arg2.capture(), any(String[].class),
-				anyString());
-		assertEquals("error should output first_name field",
-				UserAccountFields.FIRST_NAME, arg1.getValue());
-		assertEquals("show correct error code",
-				UserAccountValidator.SIGNUP_FIELD_PERSON_EXISTS,
-				arg2.getValue());
-	}
-
-	@Test
-	public void testValidatePersonIfExistsDatabaseError() throws Exception {
-		ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
-		ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
-
-		UserAccountModel userAccount = new UserAccountModel();
-		userAccount.setFirstName("firstName");
-		userAccount.setLastName("lastName");
-
-		when(workbenchDataManager
-				.isPersonExists(userAccount.getFirstName(), userAccount.getLastName()))
-				.thenThrow(MiddlewareQueryException.class);
-
-		UserAccountValidator partialValidator = spy(validator);
-		partialValidator.validatePersonIfExists(errors, userAccount);
-
-		verify(errors).rejectValue(arg1.capture(), arg2.capture());
-		assertEquals("error should output first_name field",
-				UserAccountFields.FIRST_NAME, arg1.getValue());
-		assertEquals("show correct error code",
-				UserAccountValidator.DATABASE_ERROR,
-				arg2.getValue());
-	}
-
-	@Test
-	public void testValidatePersonIfExistsNoError() throws Exception {
-		UserAccountModel userAccount = new UserAccountModel();
-		userAccount.setFirstName("firstName");
-		userAccount.setLastName("lastName");
-
-		when(workbenchDataManager
-				.isPersonExists(userAccount.getFirstName(), userAccount.getLastName()))
-				.thenReturn(false);
-
-		UserAccountValidator partialValidator = spy(validator);
-		partialValidator.validatePersonIfExists(errors, userAccount);
-
-		verify(errors, never()).rejectValue(anyString(), anyString());
-	}
-
-	@Test
 	public void testValidateUserRole() throws Exception {
 		UserAccountModel userAccount = new UserAccountModel();
 		userAccount.setRole(Role.ADMIN.name());
@@ -251,5 +196,113 @@ public class UserAccountValidatorTest {
 		assertEquals("show correct error code",
 				UserAccountValidator.SIGNUP_FIELD_INVALID_ROLE,
 				arg2.getValue());
+	}
+
+	@Test
+	public void testValidateEmailFormat() throws Exception {
+		ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
+
+		UserAccountModel userAccount = new UserAccountModel();
+		userAccount.setEmail("invalidEmail.com");
+
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator.validateEmailFormat(errors, userAccount);
+
+		verify(errors).rejectValue(arg1.capture(), arg2.capture());
+
+		assertEquals("error should output email field",
+				UserAccountFields.EMAIL, arg1.getValue());
+		assertEquals("show correct error code",
+				UserAccountValidator.SIGNUP_FIELD_INVALID_EMAIL_FORMAT,
+				arg2.getValue());
+	}
+
+	@Test
+	public void testValidateEmailFormatValid() throws Exception {
+		UserAccountModel userAccount = new UserAccountModel();
+		userAccount.setEmail("validemail123@outlook.com");
+
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator.validateEmailFormat(errors, userAccount);
+
+		verify(errors, never()).rejectValue(anyString(), anyString());
+	}
+
+	@Test
+	public void testValidateFieldLength() throws Exception {
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator
+				.validateFieldLength(errors, "invalid length", "field.prop", "Field Name", 5);
+
+		verify(errors).rejectValue(anyString(), anyString(), any(String[].class), anyString());
+	}
+
+	@Test
+	public void testValidateFieldLengthValidValue() throws Exception {
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator
+				.validateFieldLength(errors, "valid length", "field.prop", "Field Name", 30);
+
+		verify(errors, never())
+				.rejectValue(anyString(), anyString(), any(String[].class), anyString());
+	}
+
+	@Test
+	public void testValidateEmailIfExists() throws Exception {
+		ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
+
+		UserAccountModel userAccount = new UserAccountModel();
+		userAccount.setEmail("email@email.com");
+
+		when(workbenchDataManager.isPersonWithEmailExists(userAccount.getEmail())).thenReturn(true);
+
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator.validatePersonEmailIfExists(errors, userAccount);
+
+		verify(errors)
+				.rejectValue(arg1.capture(), arg2.capture());
+		assertEquals("error should output email field",
+				UserAccountFields.EMAIL, arg1.getValue());
+		assertEquals("show correct error code",
+				UserAccountValidator.SIGNUP_FIELD_EMAIL_EXISTS,
+				arg2.getValue());
+	}
+
+	@Test
+	public void testValidateEmailIfExistsDatabaseError() throws Exception {
+		ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
+
+		UserAccountModel userAccount = new UserAccountModel();
+		userAccount.setEmail("email@email.com");
+
+		when(workbenchDataManager.isPersonWithEmailExists(userAccount.getEmail())).thenThrow(
+				MiddlewareQueryException.class);
+
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator.validatePersonEmailIfExists(errors, userAccount);
+
+		verify(errors).rejectValue(arg1.capture(), arg2.capture());
+		assertEquals("error should output email field",
+				UserAccountFields.EMAIL, arg1.getValue());
+		assertEquals("show correct error code",
+				UserAccountValidator.DATABASE_ERROR,
+				arg2.getValue());
+	}
+
+	@Test
+	public void testValidateEmailIfExistsNoError() throws Exception {
+		UserAccountModel userAccount = new UserAccountModel();
+		userAccount.setUsername("email@email.com");
+
+		when(workbenchDataManager.isPersonWithEmailExists(userAccount.getEmail()))
+				.thenReturn(false);
+
+		UserAccountValidator partialValidator = spy(validator);
+		partialValidator.validatePersonEmailIfExists(errors, userAccount);
+
+		verify(errors, never()).rejectValue(anyString(), anyString());
 	}
 }
