b601647b2f133ee4110d319d4821dba8ebdeb8e1
srajwadi <sunny@leafnode.io>
2015-02-01 16:21:38 +0530

    Ontology Service to Get Standard Variables By Property
    Issue: BMS-281
    
     Reviewer: Naymesh

---
+++ src/test/java/org/generationcp/ontology/controller/IntegrationTestBase.java
@@ -0,0 +1,36 @@
+package org.generationcp.ontology.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.rules.TestName;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("/ibpworkbench-test.xml")
+public class IntegrationTestBase {
+
+    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @Rule
+    public TestName name = new TestName();
+    private long startTime;
+
+    @Before
+    public void beforeEachTest() {
+        startTime = System.nanoTime();
+    }
+
+    @After
+    public void afterEachTest() {
+        long elapsedTime = System.nanoTime() - startTime;
+        LOG.debug("+++++ Test: " + getClass().getSimpleName() + "." + name.getMethodName() + " took " + ((double) elapsedTime / 1000000)
+                + " ms = " + ((double) elapsedTime / 1000000000) + " s +++++");
+    }
+}
---
+++ src/test/java/org/generationcp/ontology/controller/OntologyServiceTest.java
@@ -0,0 +1,21 @@
+package org.generationcp.ontology.controller;
+
+import org.generationcp.middleware.domain.dms.StandardVariable;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.service.api.OntologyService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+public class OntologyServiceTest extends IntegrationTestBase{
+    
+    @Autowired
+    OntologyService ontologyService;
+    
+    @Test
+    public void callPropertyService() throws MiddlewareQueryException {
+
+        List<StandardVariable> standardVariables = ontologyService.getStandardVariablesByProperty(1);
+        assert(standardVariables.size() > 0);
+    }
+}
