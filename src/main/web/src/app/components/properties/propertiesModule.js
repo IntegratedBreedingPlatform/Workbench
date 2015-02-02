70416b174b0f11edba3d7c63fbbc88c08d6ab52d
Lisa Quayle <lisa@leafnode.io>
2015-02-02 17:03:33 +1300

    Moved the variables and properties dummy data to services.

--- src/main/web/src/app/components/properties/propertiesModule.js
+++ src/main/web/src/app/components/properties/propertiesModule.js
@@ -4,20 +4,34 @@
 (function() {
 	var app = angular.module('properties', []);
 
-	app.controller('PropertiesController', function() {
-		this.properties = [{
-			id: '23',
-			name: 'Alkali Injury',
-			description: 'Condition characterized by discoloration of the leaves ranging from white to reddish brown ' +
-				'starting from the leaf tips.',
-			classes: ['Abiotic Stress', 'Trait'],
-			cropOntologyId: 'CO_192791864'
-		}, {
-			id: '45',
-			name: 'Blast',
-			description: 'A fungus disease of rice caused by the fungus Pyricularia oryzae.',
-			classes: ['Abiotic Stress', 'Trait'],
-			cropOntologyId: 'CO_192791349'
-		}];
+	app.controller('PropertiesController', ['$scope', 'propertiesProvider', function($scope, propertiesProvider) {
+		this.properties = propertiesProvider.getProperties();
+	}]);
+
+	app.provider('propertiesProvider', function() {
+		var getProperties = function() {
+			return [{
+				id: '23',
+				name: 'Alkali Injury',
+				description: 'Condition characterized by discoloration of the leaves ranging from white to reddish brown ' +
+					'starting from the leaf tips.',
+				classes: ['Abiotic Stress', 'Trait'],
+				cropOntologyId: 'CO_192791864'
+			}, {
+				id: '45',
+				name: 'Blast',
+				description: 'A fungus disease of rice caused by the fungus Pyricularia oryzae.',
+				classes: ['Abiotic Stress', 'Trait'],
+				cropOntologyId: 'CO_192791349'
+			}];
+		};
+
+		return {
+			$get: function() {
+				return {
+					getProperties: getProperties
+				};
+			}
+		};
 	});
 }());
--- src/main/web/src/app/components/variables/variablesModule.js
+++ src/main/web/src/app/components/variables/variablesModule.js
@@ -4,45 +4,59 @@
 (function() {
 	var app = angular.module('variables', []);
 
-	app.controller('VariablesController', function() {
-		this.variables = [{
-			id: 1,
-			name: 'Plant Vigor',
-			alias: '',
-			description: 'A little vigourous',
-			property: {
-				id: '1',
-				name: 'Plant Vigor'
-			},
-			method: {
-				id: '1',
-				name: 'Visual assessment at seedling stage'
-			},
-			scale: {
-				id: '1',
-				name: 'Score',
-				dataType: '2',
-				validValues: {
-					min: '1',
-					max: '5'
+	app.controller('VariablesController', ['$scope', 'variablesProvider', function($scope, variablesProvider) {
+		this.variables = variablesProvider.getVariables();
+	}]);
+
+	app.provider('variablesProvider', function() {
+		var getVariables = function() {
+			return [{
+				id: 1,
+				name: 'Plant Vigor',
+				alias: '',
+				description: 'A little vigourous',
+				property: {
+					id: '1',
+					name: 'Plant Vigor'
+				},
+				method: {
+					id: '1',
+					name: 'Visual assessment at seedling stage'
+				},
+				scale: {
+					id: '1',
+					name: 'Score',
+					dataType: '2',
+					validValues: {
+						min: '1',
+						max: '5'
+					}
+				},
+				variableType: [
+					'1'
+				],
+				cropOntologyId: 'CO_12397f8',
+				favourite: 'true',
+				metadata: {
+					dateCreated: '2013-10-21T13:28:06.419Z',
+					lastModified: '2013-10-21T13:28:06.419Z',
+					usage: {
+						observations: '200'
+					}
+				},
+				expectedRange: {
+					min: '5',
+					max: '8'
 				}
-			},
-			variableType: [
-				'1'
-			],
-			cropOntologyId: 'CO_12397f8',
-			favourite: 'true',
-			metadata: {
-				dateCreated: '2013-10-21T13:28:06.419Z',
-				lastModified: '2013-10-21T13:28:06.419Z',
-				usage: {
-					observations: '200'
-				}
-			},
-			expectedRange: {
-				min: '5',
-				max: '8'
+			}];
+		};
+
+		return {
+			$get: function() {
+				return {
+					getVariables: getVariables
+				};
 			}
-		}];
+		};
 	});
 }());
