620b7c885a94c405f93245fe9ed338fb4272cc8f
Helen Durrant <hh.durrant@gmail.com>
2015-02-03 13:45:35 +1300

    Fixing up angular watch globs to ensure they look in nested directories.
    Issue: BMS-282
    Reviewer: Lisa Q

--- src/main/web/gulp/tasks/watch.js
+++ src/main/web/gulp/tasks/watch.js
@@ -10,9 +10,9 @@ gulp.task('watch', ['build'], function() {
 	gulp.watch('src/js/**', ['js']);
 
 	// Angular
-	gulp.watch('src/apps/**.js', ['angularJs']);
-	gulp.watch('src/apps/**.scss', ['angularSass']);
-	gulp.watch('src/apps/**.html', ['angularPages', 'angularViews']);
+	gulp.watch('src/apps/**/*.js', ['angularJs']);
+	gulp.watch('src/apps/**/*.scss', ['angularSass']);
+	gulp.watch('src/apps/**/*.html', ['angularPages', 'angularViews']);
 
 	// Live update
 	gulp.watch('../webapp/WEB-INF/**', ['hotswap']);
