5a4a79ac3c6b28d611807b44735d6930d74b47ba
Lisa Quayle <lisa@leafnode.io>
2015-02-05 09:54:51 +1300

    Added ontology module routing test to check controllers are correct.
    Issue: BMS-282
    Reviewer: Helen

--- src/main/web/test/ontologyModuleTest.js
+++ src/main/web/test/ontologyModuleTest.js
@@ -3,16 +3,34 @@
 
 describe('Ontology module', function() {
 
+	var PROPERTIES = '/properties',
+		VARIABLES = '/variables';
+
 	beforeEach(module('ontology'));
 
 	it('should map routes to views', function() {
 		inject(function($route) {
 
-			expect($route.routes['/properties'].templateUrl)
+			expect($route.routes[PROPERTIES].templateUrl)
 				.toEqual('../static/views/ontology/propertiesView.html');
 
-			expect($route.routes['/variables'].templateUrl)
+			expect($route.routes[VARIABLES].templateUrl)
 				.toEqual('../static/views/ontology/variablesView.html');
+		});
+	});
+
+	it('should map routes to controllers', function() {
+		inject(function($route) {
+
+			expect($route.routes[PROPERTIES].controller)
+				.toEqual('PropertiesController');
+			expect($route.routes[PROPERTIES].controllerAs)
+				.toEqual('propsCtrl');
+
+			expect($route.routes[VARIABLES].controller)
+				.toEqual('VariablesController');
+			expect($route.routes[VARIABLES].controllerAs)
+				.toEqual('varsCtrl');
 		});
 	});
 });
