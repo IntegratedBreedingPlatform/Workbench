9a7f4ceefd5cc97de116cb36e8ebca5e8b80f081
Helen Durrant <hh.durrant@gmail.com>
2015-02-05 14:40:03 +1300

    Adding a test task and coverage reporting.
    Issue: BMS-300
    Reviewer: Lisa Q

---
+++ src/main/web/karma.conf.ci.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var baseConfig = require('./karma.conf.js');
+
+module.exports = function (config) {
+	baseConfig(config);
+
+	// Override base config
+	config.set({
+		logLevel: config.LOG_DEBUG,
+		singleRun: true,
+		colors: false,
+		autoWatch: false,
+		reporters: ['progress', 'junit', 'coverage'],
+		preprocessors: {
+			'../webapp/WEB-INF/static/views/**/*.html': 'ng-html2js',
+			'src/apps/**/*.js': ['coverage']
+		},
+		junitReporter: {
+			outputFile: 'reports/junit/js-tests.xml'
+		},
+		coverageReporter: {
+			type: 'lcov',
+			dir: 'reports',
+			subdir: 'coverage'
+		}
+	});
+};
