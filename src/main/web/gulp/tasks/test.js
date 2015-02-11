bedaf6bf85ca5b52b58353c99de3d737430a190d
Helen Durrant <hh.durrant@gmail.com>
2015-02-11 15:07:42 +1300

    Attempting to fix up sonar to see test coverage.
    Issue: BMS-300
    Reviewer: Lisa Q

--- src/main/web/gulp/tasks/test.js
+++ src/main/web/gulp/tasks/test.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var gulp = require('gulp'),
+	argv = require('yargs').argv,
+	path = require('path'),
 	karma = require('karma').server;
 
 gulp.task('test', function (done) {
+	var karmaConf = argv.ci ? 'karma.conf.ci.js' : 'karma.conf.js';
+
 	karma.start({
-		configFile: __dirname + '/../../karma.conf.js',
+		configFile: path.join(__dirname, '/../../', karmaConf),
 		singleRun: true
 	}, done);
 });
