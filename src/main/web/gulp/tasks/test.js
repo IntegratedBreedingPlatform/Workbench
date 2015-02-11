aa6bf05e03f2c1c754575a8e90692887b98261ce
Helen Durrant <hh.durrant@gmail.com>
2015-02-12 10:21:44 +1300

    So close.. just getting JS unit test results to be read in.
    Issue: BMS-300
    Reviewer: Lisa Q

--- src/main/web/gulp/tasks/test.js
+++ src/main/web/gulp/tasks/test.js
@@ -4,17 +4,24 @@ var gulp = require('gulp'),
 	argv = require('yargs').argv,
 	path = require('path'),
 	karma = require('karma').server,
-	replace = require('gulp-replace');
+	replace = require('gulp-replace'),
+	merge = require('merge-stream');
 
-function postProcessLCOV() {
-	return gulp.src('reports/coverage/lcov.info')
-		.pipe(replace('SF:.', 'SF:src/main/web'))
-		.pipe(gulp.dest('reports/coverage'));
+function postProcessReports() {
+	var lcov = gulp.src('reports/coverage/lcov.info')
+			.pipe(replace('SF:.', 'SF:src/main/web'))
+			.pipe(gulp.dest('reports/coverage')),
+
+		jsunit = gulp.src('reports/junit/TESTS-junit.xml')
+			.pipe(replace('test_', 'test.'))
+			.pipe(gulp.dest('reports/junit'));
+
+	return merge(lcov, jsunit);
 }
 
 gulp.task('test', function (done) {
 	var karmaConf = argv.ci ? 'karma.conf.ci.js' : 'karma.conf.js',
-		cb = argv.ci ? postProcessLCOV : done;
+		cb = argv.ci ? postProcessReports : done;
 
 	karma.start({
 		configFile: path.join(__dirname, '/../../', karmaConf),
---
+++ src/main/web/test/variablesServiceTest.js
@@ -0,0 +1,76 @@
+/*global expect, inject*/
+'use strict';
+
+describe('test.variablesServiceTest', function() {
+	var variablesServiceUrl = 'http://private-905fc7-ontologymanagement.apiary-mock.com/variables',
+		variablesService,
+		httpBackend;
+
+	beforeEach(function() {
+		module('variables');
+
+		inject(function(_variablesService_, $httpBackend) {
+			variablesService = _variablesService_;
+			httpBackend = $httpBackend;
+		});
+	});
+
+	afterEach(function() {
+		httpBackend.verifyNoOutstandingExpectation();
+		httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('Should return an array of objects', function() {
+		var variables = [{
+				name: 'Var1',
+				description: 'This is var1'
+			}, {
+				name: 'Var2',
+				description: 'This is var2'
+			}],
+			result;
+
+		httpBackend.expectGET(variablesServiceUrl).respond(variables);
+
+		variablesService.getVariables().then(function(response) {
+			result = response;
+		});
+
+		httpBackend.flush();
+
+		expect(result instanceof Array).toBeTruthy();
+		expect(result).toEqual(variables);
+	});
+
+	it('Should reject the promise when 500 response recieved', function() {
+		var result;
+
+		httpBackend.expectGET(variablesServiceUrl).respond(500);
+
+		variablesService.getVariables().then(function(response) {
+			result = response;
+		}, function(reason) {
+			result = reason;
+		});
+
+		httpBackend.flush();
+
+		expect(result).toEqual('An unknown error occurred.');
+	});
+
+	it('Should reject the promise when 400 response recieved', function() {
+		var result;
+
+		httpBackend.expectGET(variablesServiceUrl).respond(400);
+
+		variablesService.getVariables().then(function(response) {
+			result = response;
+		}, function(reason) {
+			result = reason;
+		});
+
+		httpBackend.flush();
+
+		expect(result).toEqual('Request was malformed.');
+	});
+});
