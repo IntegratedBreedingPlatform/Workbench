7a35e8a873e028af37986f57f7e7e55e0c013040
Lisa Quayle <lisa@leafnode.io>
2015-02-25 08:56:17 +1300

    Indented function.
    Issue: BMS-282
    Reviewer: Helen

--- src/main/web/src/apps/ontology/app-components/add-variable/addVariableModule.js
+++ src/main/web/src/apps/ontology/app-components/add-variable/addVariableModule.js
@@ -7,55 +7,56 @@
 	app.controller('AddVariableController', ['$scope', '$location', 'addVariableService', 'propertiesService',
 		function($scope, $location, addVariableService, propertiesService) {
 
-		$scope.methods = [];
-		$scope.scales = [];
-		$scope.types = [];
+			$scope.methods = [];
+			$scope.scales = [];
+			$scope.types = [];
 
-		$scope.properties = [];
+			$scope.properties = [];
 
-		// Restore state in case we were half way through creating variable
-		$scope.variable = angular.copy(addVariableService.getVariableState());
+			// Restore state in case we were half way through creating variable
+			$scope.variable = angular.copy(addVariableService.getVariableState());
 
-		propertiesService.getProperties().then(function(properties) {
-			$scope.properties = properties;
+			propertiesService.getProperties().then(function(properties) {
+				$scope.properties = properties;
 
-			// FIXME - Change to ID
-			// Select the currently selected property if there is one
-			if ($scope.variable.property) {
-				$scope.properties.some(function(prop) {
-					if (prop.name === $scope.variable.property.name) {
-						$scope.variable.property = prop;
-						return true;
-					}
-				});
-			}
-		});
+				// FIXME - Change to ID
+				// Select the currently selected property if there is one
+				if ($scope.variable.property) {
+					$scope.properties.some(function(prop) {
+						if (prop.name === $scope.variable.property.name) {
+							$scope.variable.property = prop;
+							return true;
+						}
+					});
+				}
+			});
 
-		$scope.numericVariable = true;
+			$scope.numericVariable = true;
 
-		$scope.saveVariable = function(e, variable) {
-			e.preventDefault();
-			addVariableService.saveVariable(variable);
-		};
+			$scope.saveVariable = function(e, variable) {
+				e.preventDefault();
+				addVariableService.saveVariable(variable);
+			};
 
-		$scope.addProperty = function(e, variable) {
-			e.preventDefault();
+			$scope.addProperty = function(e, variable) {
+				e.preventDefault();
 
-			addVariableService.updateVariableState(variable);
+				addVariableService.updateVariableState(variable);
 
-			$location.path('/add/property');
-		};
+				$location.path('/add/property');
+			};
 
-		$scope.addMethod = function(e) {
-			e.preventDefault();
-			alert('Add method');
-		};
+			$scope.addMethod = function(e) {
+				e.preventDefault();
+				alert('Add method');
+			};
 
-		$scope.addScale = function(e) {
-			e.preventDefault();
-			alert('Add scale');
-		};
-	}]);
+			$scope.addScale = function(e) {
+				e.preventDefault();
+				alert('Add scale');
+			};
+		}
+	]);
 
 	app.service('addVariableService', [function() {
 
