14c8c1470703592148fdd8f4c7bc390efb0b75bd
Helen Durrant <hh.durrant@gmail.com>
2015-02-25 11:04:28 +1300

    Adding more list module tests.
    Issue: BMS-282
    Reviewer: Lisa Q

--- src/main/web/test/listModuleTest.js
+++ src/main/web/test/listModuleTest.js
@@ -1,14 +1,22 @@
-/*global angular, inject, expect*/
+/*global angular, inject, expect, spyOn*/
 'use strict';
 
 describe('List module', function() {
 	var LIST_ITEM_CAT = {
+			id: 'cat',
 			name: 'Cat',
-			description: 'A fluffy animal that likes to sleep.'
+			description: 'A fluffy animal that likes to sleep.',
+			'action-favourite': {
+				iconValue: 'iconFavourite'
+			}
 		},
 		LIST_ITEM_DOG = {
+			id: 'dog',
 			name: 'Dog',
-			description: 'A playful animal that likes walks'
+			description: 'A playful animal that likes walks',
+			'action-favourite': {
+				iconValue: 'icon'
+			}
 		},
 
 		scope,
@@ -25,9 +33,11 @@ describe('List module', function() {
 		scope = $rootScope.$new();
 	}));
 
-	function compileDirective() {
+	function compileDirective(extraAttrs) {
+		var attrs = extraAttrs || '';
+
 		inject(function($compile) {
-			directiveElement = $compile('<om-list om-data="testData" om-col-headers="testHeaders"></om-list>')(scope);
+			directiveElement = $compile('<om-list om-data="testData" om-col-headers="testHeaders" ' + attrs + '></om-list>')(scope);
 		});
 
 		scope.$digest();
@@ -72,4 +82,62 @@ describe('List module', function() {
 		expect(directiveElement.find('.om-li-data-cell-test')[1]).toContainText(LIST_ITEM_CAT[secondHeader]);
 	});
 
+
+	it('should only display column header if header is not an action header', function() {
+
+		var normalHeader = 'name',
+			actionHeader = 'action-favourite';
+
+		scope.testHeaders = [normalHeader, actionHeader];
+
+		compileDirective();
+
+		expect(directiveElement.find('.om-li-header-cell-test').length).toEqual(1);
+		expect(directiveElement.find('.om-li-header-cell-test')[0]).toContainText(normalHeader);
+	});
+
+	it('should display column values for non action columns and iconValues for action columns', function() {
+
+		var normalHeader = 'name',
+			actionHeader = 'action-favourite';
+
+		scope.testHeaders = [normalHeader, actionHeader];
+		scope.testData = [LIST_ITEM_CAT];
+
+		compileDirective();
+
+		expect(directiveElement.find('.om-li-data-cell-test')[0]).toContainText(LIST_ITEM_CAT[normalHeader]);
+		expect(directiveElement.find('.om-li-data-cell-test')[1]).toContainText(LIST_ITEM_CAT[actionHeader].iconValue);
+	});
+
+	describe('scope.clickHandler', function() {
+
+		it('should call the parent click handler and set the selected item id', function() {
+
+			var firstHeader = 'name',
+				secondHeader = 'description',
+
+				item = {
+					id: null
+				},
+
+				count = 0;
+
+			scope.testHeaders = [firstHeader, secondHeader];
+			scope.testData = [LIST_ITEM_CAT];
+
+			scope.clickFn = function() {
+				count++;
+			};
+			scope.selectedItem = item;
+
+			compileDirective('om-on-click="clickFn()" om-selected-item="selectedItem"');
+
+			// Call the click handler that would normally be invoked by a click on a list item
+			directiveElement.isolateScope().clickHandler(LIST_ITEM_CAT.id);
+
+			expect(count).toEqual(1);
+			expect(item.id).toEqual(LIST_ITEM_CAT.id);
+		});
+	});
 });
