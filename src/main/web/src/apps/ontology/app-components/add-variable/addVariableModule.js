b336e45f8199091cfbab2b8f74f5d00e9298d25a
Helen Durrant <hh.durrant@gmail.com>
2015-02-24 17:17:05 +1300

    First cut of add variables model / service - enabling persistence of state even if the user goes to create a new property / method / scale while in the middle of adding a variable.
    Issue: BMS-282
    Reviewer: Lisa Q

--- src/main/web/src/apps/ontology/app-components/add-variable/addVariableModule.js
+++ src/main/web/src/apps/ontology/app-components/add-variable/addVariableModule.js
@@ -2,23 +2,37 @@
 'use strict';
 
 (function() {
-	var app = angular.module('addVariable', []);
+	var app = angular.module('addVariable', ['properties']);
 
-	app.controller('AddVariableController', ['$scope', '$location', function($scope, $location) {
-		this.properties = [];
-		this.methods = [];
-		this.scales = [];
-		this.types = [];
+	app.controller('AddVariableController', ['$scope', '$location', 'addVariableService', 'propertiesService',
+		function($scope, $location, addVariableService, propertiesService) {
+
+		var ctrl = this;
+
+		ctrl.properties = [];
+		ctrl.methods = [];
+		ctrl.scales = [];
+		ctrl.types = [];
+
+		propertiesService.getProperties().then(function(properties) {
+			ctrl.properties = properties;
+		});
 
 		$scope.numericVariable = true;
 
-		$scope.saveVariable = function(e) {
+		// Restore state in case we were half way through creating variable
+		$scope.variable = angular.copy(addVariableService.getVariableState());
+
+		$scope.saveVariable = function(e, variable) {
 			e.preventDefault();
-			alert('Save variable');
+			addVariableService.saveVariable(variable);
 		};
 
-		$scope.addProperty = function(e) {
+		$scope.addProperty = function(e, variable) {
 			e.preventDefault();
+
+			addVariableService.updateVariableState(variable);
+
 			$location.path('/add/property');
 		};
 
@@ -30,6 +44,29 @@
 		$scope.addScale = function(e) {
 			e.preventDefault();
 			alert('Add scale');
+		};
+	}]);
+
+	app.service('addVariableService', [function() {
+
+		var variable = {};
+
+		return {
+			updateVariableState: function(updatedVariable) {
+				variable = angular.copy(updatedVariable);
+			},
+
+			saveVariable: function(variable) {
+				// TODO Call actual save functionality
+				console.log('Saving variable');
+
+				// If successful..
+				variable = {};
+			},
+
+			getVariableState: function() {
+				return variable;
+			}
 		};
 	}]);
 
