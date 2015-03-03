ea1c00374ebe6ab30b4df4e884514cdb323f7e8e
Helen Durrant <hh.durrant@gmail.com>
2015-03-03 16:17:59 +1300

    Adding to the properties view test.
    Issue: BMS-282
    Reviewer: Lisa Q

--- src/main/web/test/app-components/propertiesViewTest.js
+++ src/main/web/test/app-components/propertiesViewTest.js
@@ -2,11 +2,19 @@
 'use strict';
 
 describe('Properties View', function() {
-	var q,
+	var BLAST = {
+			name: 'Blast',
+			description: 'I\'ts a blast',
+			classes: ['class', 'anotherClass']
+		},
+
+		q,
 		controller,
 		scope,
-		deferred,
-		propertiesService;
+		deferredGetProperties,
+		deferredGetProperty,
+		propertiesService,
+		panelService;
 
 	beforeEach(function() {
 		module('propertiesView');
@@ -15,17 +23,29 @@ describe('Properties View', function() {
 	beforeEach(inject(function($q, $controller, $rootScope) {
 		propertiesService = {
 			getProperties: function() {
-				deferred = q.defer();
-				return deferred.promise;
+				deferredGetProperties = q.defer();
+				return deferredGetProperties.promise;
+			},
+			getProperty: function() {
+				deferredGetProperty = q.defer();
+				return deferredGetProperty.promise;
 			}
 		};
+
+		panelService = {
+			showPanel: function() {}
+		};
+
 		spyOn(propertiesService, 'getProperties').and.callThrough();
+		spyOn(propertiesService, 'getProperty').and.callThrough();
+		spyOn(panelService, 'showPanel');
 
 		q = $q;
 		scope = $rootScope;
 		controller = $controller('PropertiesController', {
 			$scope: scope,
-			propertiesService: propertiesService
+			propertiesService: propertiesService,
+			panelService: panelService
 		});
 	}));
 
@@ -41,9 +61,38 @@ describe('Properties View', function() {
 				Classes: 'class1, class2'
 			}];
 
-		deferred.resolve(jsonData);
+		deferredGetProperties.resolve(jsonData);
 		scope.$apply();
 		expect(propertiesService.getProperties).toHaveBeenCalled();
 		expect(controller.properties).toEqual(transformedData);
+	});
+
+	it('should set the selected item to be an object with an id property set to null by default', function() {
+		expect(scope.selectedItem).toEqual({id: null});
+	});
+
+	it('should set the selected property to be null by default', function() {
+		expect(scope.selectedProperty).toEqual(null);
+	});
+
+	describe('$scope.showPropertyDetails', function() {
+
+		it('should retrieve the selected property and display the panel', function() {
+
+			var selectedId = 123,
+				panelName = 'properties',
+				property = BLAST;
+
+			scope.selectedItem.id = selectedId;
+			scope.panelName = panelName;
+
+			scope.showPropertyDetails();
+			deferredGetProperty.resolve(property);
+			scope.$apply();
+
+			expect(propertiesService.getProperty).toHaveBeenCalledWith(selectedId);
+			expect(scope.selectedProperty).toEqual(property);
+			expect(panelService.showPanel).toHaveBeenCalledWith(panelName);
+		});
 	});
 });
