32c34a24c7b71cc9c1f94503570c87dd291edbfb
Helen Durrant <hh.durrant@gmail.com>
2015-03-12 10:13:06 +1300

    Updating the add prop screen to ensure the property object we pass to the add property method is in the appropriate format. Adding an update method to the properties service ready for use by the edit property screen.
    Issue: BMS-289, BMS-286
    Reviewer: Lisa Q

--- src/main/web/src/apps/ontology/shared-services/properties.js
+++ src/main/web/src/apps/ontology/shared-services/properties.js
@@ -10,25 +10,101 @@
 			failureHandler = serviceUtilities.restFailureHandler;
 
 		return {
-			// Properties services (plural)
+			/*
+			Returns an array of properties in the format:
+
+			[{
+				'id': 23,
+				'name': 'Alkali Injury',
+				'description': 'Condition characterized by discoloration of the leaves ranging from ...',
+				'classes': ['Abiotic Stress', 'Trait'],
+				'cropOntologyId': 'CO_192791864'
+			}]
+
+			*/
 			getProperties: function() {
 				var request = $http.get('http://private-905fc7-ontologymanagement.apiary-mock.com/bmsapi/ontology/rice/properties');
 				return request.then(successHandler, failureHandler);
 			},
 
+			/*
+			Expects a property in the format:
+
+			{
+				'name': 'Blast',
+				'description': 'A fungus disease of rice caused by the fungus Pyricularia oryzae.',
+				'classes': ['Abiotic Stress', 'Trait']
+				'cropOntologyId': 'CO_192791349'
+			}
+
+			If the response has a 400 status, the response data will contain an errors property which is an
+			array of error objects, each with a message and optional fieldName, linking the message to a
+			specific field on the page (by HTML name).
+
+			{
+				'errors': [{
+					'fieldNames': ['name'],
+					'message': 'A property with that name already exists.'
+				}]
+			}
+			*/
 			addProperty: function(property) {
 				var request = $http.post('http://private-f74035-ontologymanagement.apiary-mock.com/bmsapi/ontology/rice/properties',
 					property);
 				return request.then(successHandler, failureHandler);
 			},
 
-			// Property services (on a specific property)
+			/*
+			Expects a property in the format:
+
+			{
+				'name': 'Plant Height',
+				'description': 'The plant height',
+				'classes': ['Trait']
+				'cropOntologyId': 'CO_192791864'
+			}
+
+			If the response has a 400 status, the response data will contain an errors property which is an
+			array of error objects, each with a message and optional fieldName, linking the message to a
+			specific field on the page (by HTML name).
+
+			{
+				'errors': [{
+					'fieldNames': ['name'],
+					'message': 'A property with that name already exists.'
+				}]
+			}
+			*/
+			updateProperty: function(id, property) {
+				var request = $http.put('http://private-f74035-ontologymanagement.apiary-mock.com/bmsapi/ontology/rice/properties/:id',
+					property);
+				return request.then(function(response) {
+					return response.status;
+				}, failureHandler);
+			},
+
+			/*
+			Returns a single property in the format:
+
+			{
+				'name': 'Alkali Injury',
+				'description': 'Condition characterized by discoloration of the leaves ranging from ...',
+				'classes': ['Abiotic Stress', 'Trait'],
+				'cropOntologyId': 'CO_192791864',
+				'editableFields': ['name', 'description', 'classes', 'cropOntologyId'],
+				'deletable': true
+			}
+			*/
 			getProperty: function(/*id*/) {
 				var request = $http.get('http://private-905fc7-ontologymanagement.apiary-mock.com/bmsapi/ontology/rice/properties/:id');
 				return request.then(successHandler, failureHandler);
 			},
 
-			// Classes services (plural)
+			/*
+			Returns an array of classes in the format:
+
+			['Abiotic Stress', 'Agronomic', 'Biotic Stress', 'Germplasm']
+			*/
 			getClasses: function() {
 				var request = $http.get('http://private-f74035-ontologymanagement.apiary-mock.com/bmsapi/ontology/rice/classes');
 				return request.then(successHandler, failureHandler);
