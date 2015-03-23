c22c2a69a59814d0dbe179b097fbe9de48b0f150
Naymesh <naymesh@leafnode.io>
2015-03-24 11:21:02 +1300

    Exposing currently selected (program's) crop name to the main web view.
    Issue: BMS-36
    Reviewer: Helen D

--- src/main/java/org/generationcp/ibpworkbench/controller/OntologyController.java
+++ src/main/java/org/generationcp/ibpworkbench/controller/OntologyController.java
@@ -1,15 +1,23 @@
 package org.generationcp.ibpworkbench.controller;
 
+import org.generationcp.commons.spring.util.ContextUtil;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/ontology")
 public class OntologyController {
+	
+	@Autowired
+	private ContextUtil context;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String ontology(){
+	public String ontology(Model model) throws MiddlewareQueryException {
+		model.addAttribute("currentCrop", context.getProjectInContext().getCropType().getCropName());
 		return "ontology";
 	}
 }
