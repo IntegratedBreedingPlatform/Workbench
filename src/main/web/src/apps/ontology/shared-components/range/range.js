532164a302430dd1f7179fdb3cde7e9c2abca33c
Helen Durrant <hh.durrant@gmail.com>
2015-03-27 11:31:39 +1300

    Adding range validation.
    Issue: BMS-284
    Reviewer: Lisa Q

--- src/main/web/src/apps/ontology/shared-components/range/range.js
+++ src/main/web/src/apps/ontology/shared-components/range/range.js
@@ -13,12 +13,56 @@
 				}
 				$scope.editable = editable($scope);
 			},
+			link: function(scope, elm, attrs, ctrl) {
+
+				var resetValidity = function() {
+					ctrl.$setValidity('mustProvideBoth', true);
+					ctrl.$setValidity('minTooBig', true);
+					ctrl.$setValidity('minOutOfRange', true);
+					ctrl.$setValidity('maxOutOfRange', true);
+				};
+
+				scope.$watch('numeric', function (numeric) {
+					if (!numeric) {
+						resetValidity();
+					}
+				});
+
+				scope.$watch('model[property]', function (data) {
+					resetValidity();
+
+					if (!scope.numeric) {
+						return;
+					}
+
+					if (data.max && !data.min || !data.max && data.min) {
+						ctrl.$setValidity('mustProvideBoth', false);
+					}
+
+					if (data.max && data.min && data.max <= data.min) {
+						ctrl.$setValidity('minTooBig', false);
+					}
+
+					if (scope.min && data.min && data.min < scope.min || data.min > scope.max) {
+						ctrl.$setValidity('minOutOfRange', false);
+					}
+
+					if (scope.max && data.max && data.max > scope.max || data.max < scope.min) {
+						ctrl.$setValidity('maxOutOfRange', false);
+					}
+
+				}, true);
+			},
+			require: 'ngModel',
 			restrict: 'E',
 			scope: {
+				min: '@omMin',
+				max: '@omMax',
+				numeric: '=omNumeric',
 				property: '@omProperty',
 				adding: '=omAdding',
 				editing: '=omEditing',
-				model: '=omModel'
+				model: '=ngModel'
 			},
 			templateUrl: 'static/views/ontology/range.html'
 		};
--- src/main/web/test/shared-components/rangeTest.js
+++ src/main/web/test/shared-components/rangeTest.js
@@ -30,7 +30,7 @@ describe('Range module', function() {
 
 	function compileDirective() {
 		inject(function($compile) {
-			directiveElement = $compile('<om-range om-model="model" om-property="validValues"></om-range>')(scope);
+			directiveElement = $compile('<om-range ng-model="model" om-property="validValues"></om-range>')(scope);
 		});
 
 		scope.$digest();
@@ -53,5 +53,129 @@ describe('Range module', function() {
 		compileDirective();
 
 		expect(scope.model).toBeUndefined();
+	});
+
+	describe('Range validation', function() {
+
+		function compileForm(extraAttrs) {
+			var attrs = extraAttrs || '';
+
+			inject(function($compile) {
+				directiveElement = $compile(
+					'<form name="testForm" novalidate>' +
+						'<om-range name="omRange" ng-model="model" om-property="validValues" ' + attrs + '></om-range>' +
+					'</form>'
+					)(scope);
+			});
+
+			scope.$digest();
+		}
+
+		it('should set the widget to be valid if the selected data type is not numeric', function() {
+
+			compileForm('om-numeric="false"');
+
+			expect(scope.testForm.omRange.$error).toEqual({});
+			expect(scope.testForm.$valid).toBe(true);
+		});
+
+		it('should set the mustProvideBoth error to be true if a minimum is provided but there is no maximum', function() {
+
+			scope.model = {
+				validValues: {
+					max: 90
+				}
+			};
+
+			compileForm('om-numeric="true"');
+
+			expect(scope.testForm.omRange.$error).toEqual({mustProvideBoth: true});
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the mustProvideBoth error to be true if a maximum is provided but there is no minimum', function() {
+
+			scope.model = {
+				validValues: {
+					min: 80
+				}
+			};
+
+			compileForm('om-numeric="true"');
+
+			expect(scope.testForm.omRange.$error).toEqual({mustProvideBoth: true});
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the minTooBig error to be true if a minimum is provided and it exceeds the maximum', function() {
+
+			scope.model = {
+				validValues: {
+					min: 90,
+					max: 80
+				}
+			};
+
+			compileForm('om-numeric="true"');
+
+			expect(scope.testForm.omRange.$error).toEqual({minTooBig: true});
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the minOutOfRange error to be true if the minimum falls below the allowed range', function() {
+
+			scope.model = {
+				validValues: {
+					min: 80
+				}
+			};
+
+			compileForm('om-numeric="true" om-min="90" om-max="100"');
+
+			expect(scope.testForm.omRange.$error.minOutOfRange).toBe(true);
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the minOutOfRange error to be true if the minimum falls above the allowed range', function() {
+
+			scope.model = {
+				validValues: {
+					min: 80
+				}
+			};
+
+			compileForm('om-numeric="true" om-min="60" om-max="70"');
+
+			expect(scope.testForm.omRange.$error.minOutOfRange).toBe(true);
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the maxOutOfRange error to be true if the maximum falls below the allowed range', function() {
+
+			scope.model = {
+				validValues: {
+					max: 80
+				}
+			};
+
+			compileForm('om-numeric="true" om-min="90" om-max="100"');
+
+			expect(scope.testForm.omRange.$error.maxOutOfRange).toBe(true);
+			expect(scope.testForm.$valid).toBe(false);
+		});
+
+		it('should set the maxOutOfRange error to be true if the maximum falls above the allowed range', function() {
+
+			scope.model = {
+				validValues: {
+					max: 80
+				}
+			};
+
+			compileForm('om-numeric="true" om-min="60" om-max="70"');
+
+			expect(scope.testForm.omRange.$error.maxOutOfRange).toBe(true);
+			expect(scope.testForm.$valid).toBe(false);
+		});
 	});
 });
