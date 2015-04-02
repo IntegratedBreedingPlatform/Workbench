8bfa49d09fbc26ceafadafd55f49e930e875afb5
Lisa Quayle <lisa@leafnode.io>
2015-04-02 13:20:32 +1300

    Removed unused directives.
    Issue: none
    Reviewer: Helen

--- src/main/web/src/apps/ontology/shared-components/form-fields/formFields.js
+++ src/main/web/src/apps/ontology/shared-components/form-fields/formFields.js
@@ -14,44 +14,6 @@
 		};
 	});
 
-	formFieldsModule.directive('omMultiUiSelect', function(editable) {
-		return {
-			controller: function ($scope) {
-				$scope.editable = editable($scope);
-			},
-			restrict: 'E',
-			scope: {
-				// omOptions must be an array of objects with (at least) name and id properties.
-				options: '=omOptions',
-				id: '@omId',
-				label: '@omLabel',
-				property: '@omProperty',
-				editing: '=omEditing',
-				model: '=omModel'
-			},
-			templateUrl: 'static/views/ontology/multiUiSelect.html'
-		};
-	});
-
-	formFieldsModule.directive('omTagUiSelect', function(editable) {
-		return {
-			controller: function ($scope) {
-				$scope.editable = editable($scope);
-			},
-			restrict: 'E',
-			scope: {
-				// omOptions must be an array of strings.
-				options: '=omOptions',
-				id: '@omId',
-				label: '@omLabel',
-				property: '@omProperty',
-				editing: '=omEditing',
-				model: '=omModel'
-			},
-			templateUrl: 'static/views/ontology/tagUiSelect.html'
-		};
-	});
-
 	// Filter to return objects from an array of items by the id in the object, using the passed
 	// in id as the filter parameter.
 	formFieldsModule.filter('id', function($filter){
