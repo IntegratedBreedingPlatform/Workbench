0153999e9c4205ec6c56581387206a2b975c4559
Cyrus Venn Casada <cvpcasada@outlook.com>
2015-09-17 13:28:12 +0800

    Removed padded <p/> tag

--- src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/WorkbenchMainView.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2012, All Rights Reserved.
- * <p/>
+ *
  * Generation Challenge Programme (GCP)
- * <p/>
- * <p/>
+ *
+ *
  * This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of Part F
  * of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
  *******************************************************************************/
