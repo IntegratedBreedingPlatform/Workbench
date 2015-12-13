bff4166dc4232cabbee3db79a4b98d66f18e10bf
Mae Isabelle Turiana <maeisabelle@gmail.com>
2015-12-13 22:20:05 +0800

    Completed unit tests for the 3 kinds of alpha lattice presets
    BMS-2055
    
    Reviewer: Crisjaira Villarino

--- src/test/java/org/generationcp/ibpworkbench/ui/breedingview/singlesiteanalysis/SingleSiteAnalysisDetailsPanelTest.java
+++ src/test/java/org/generationcp/ibpworkbench/ui/breedingview/singlesiteanalysis/SingleSiteAnalysisDetailsPanelTest.java
@@ -526,9 +526,67 @@ public class SingleSiteAnalysisDetailsPanelTest {
 	}
 
 	@Test
-	public void testDesignTypeAlphaLatticePresets() {
+	public void testDesignType1stAlphaLatticePreset() {
 		Mockito.when(this.studyDataManager.getGeolocationPropValue(TermId.EXPERIMENT_DESIGN_FACTOR.getId(), this.input.getStudyId()))
 				.thenReturn(Integer.toString(TermId.ALPHA_LATTICE_E30_REP2.getId()));
+
+		this.dut.initializeComponents();
+
+		GridLayout gLayout = (GridLayout) this.dut.getBlockRowColumnContainer().getComponentIterator().next();
+		Iterator<Component> componentsIterator = gLayout.getComponentIterator();
+		List<Component> components = new ArrayList<>();
+		while (componentsIterator.hasNext()) {
+			Component component = componentsIterator.next();
+			components.add(component);
+		}
+
+		Assert.assertTrue(components.contains(this.dut.getLblBlocks()));
+		Assert.assertTrue(components.contains(this.dut.getSelBlocks()));
+		Assert.assertTrue(components.contains(this.dut.getLblSpecifyGenotypesHeader()));
+		Assert.assertTrue(components.contains(this.dut.getLblGenotypes()));
+		Assert.assertTrue(components.contains(this.dut.getSelGenotypes()));
+
+		Assert.assertFalse(components.contains(this.dut.getLblSpecifyColumnFactor()));
+		Assert.assertFalse(components.contains(this.dut.getSelColumnFactor()));
+		Assert.assertFalse(components.contains(this.dut.getLblSpecifyRowFactor()));
+		Assert.assertFalse(components.contains(this.dut.getSelRowFactor()));
+
+		Assert.assertTrue(this.dut.getSelDesignType().getValue().equals(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN.getName()));
+	}
+
+	@Test
+	public void testDesignType2ndAlphaLatticePresets() {
+		Mockito.when(this.studyDataManager.getGeolocationPropValue(TermId.EXPERIMENT_DESIGN_FACTOR.getId(), this.input.getStudyId()))
+				.thenReturn(Integer.toString(TermId.ALPHA_LATTICE_E30_REP3.getId()));
+
+		this.dut.initializeComponents();
+
+		GridLayout gLayout = (GridLayout) this.dut.getBlockRowColumnContainer().getComponentIterator().next();
+		Iterator<Component> componentsIterator = gLayout.getComponentIterator();
+		List<Component> components = new ArrayList<>();
+		while (componentsIterator.hasNext()) {
+			Component component = componentsIterator.next();
+			components.add(component);
+		}
+
+		Assert.assertTrue(components.contains(this.dut.getLblBlocks()));
+		Assert.assertTrue(components.contains(this.dut.getSelBlocks()));
+		Assert.assertTrue(components.contains(this.dut.getLblSpecifyGenotypesHeader()));
+		Assert.assertTrue(components.contains(this.dut.getLblGenotypes()));
+		Assert.assertTrue(components.contains(this.dut.getSelGenotypes()));
+
+		Assert.assertFalse(components.contains(this.dut.getLblSpecifyColumnFactor()));
+		Assert.assertFalse(components.contains(this.dut.getSelColumnFactor()));
+		Assert.assertFalse(components.contains(this.dut.getLblSpecifyRowFactor()));
+		Assert.assertFalse(components.contains(this.dut.getSelRowFactor()));
+
+		Assert.assertTrue(this.dut.getSelDesignType().getValue().equals(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN.getName()));
+	}
+
+	@Test
+	public void testDesignType3rdAlphaLatticePresets() {
+		Mockito.when(this.studyDataManager.getGeolocationPropValue(TermId.EXPERIMENT_DESIGN_FACTOR.getId(), this.input.getStudyId()))
+				.thenReturn(Integer.toString(TermId.ALPHA_LATTICE_E50_REP2.getId()));
 
 		this.dut.initializeComponents();
 
