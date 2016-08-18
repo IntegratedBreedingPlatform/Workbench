95ad51662b6c49d9023f50d46dbce32997a5002b
Nahuel Soldevilla <nsoldevilla@droptek.com>
2016-08-18 12:41:11 -0300

    Basic Angular 2 Proof of Concept
    A hello world type application that leverages the current gulp build
    tasks used by the angular 1 ontology app.
    There is no UI access to this app yet so a manual URL change must be
    made using the browser developer tools after entering the ontology
    manager. The URL must be changed for 
    /ibpworkbench/controller/angular2poc
    
    Reviewer: Clarysabel
    Issue: BMS-3154 / BMS-3159

---
+++ src/main/java/org/generationcp/ibpworkbench/controller/Angular2POCController.java
@@ -0,0 +1,18 @@
+
+package org.generationcp.ibpworkbench.controller;
+
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/angular2poc")
+public class Angular2POCController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String angular2poc(Model model) throws MiddlewareQueryException {
+		return "angular2-poc";
+	}
+}
