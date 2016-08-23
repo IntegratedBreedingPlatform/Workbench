b393e4b0947004ad6b00d6d103c0f589b4f6ee77
Nahuel Soldevilla <nsoldevilla@droptek.com>
2016-08-23 13:47:00 -0300

    Advance with Angular 2 Proof of Concept
    - Fix system js so the app can load
    - ignore typings folder
    - Allow spring security access on controller/pages/angular2/lib/ folder
    - Map pages/angular2 on spring mvs resources
    
    
    Issue: BMS-3154 / BMS-3159

--- src/main/angular2/src/apps/angular2-poc/systemjs.config.js
+++ src/main/angular2/src/apps/angular2-poc/systemjs.config.js
@@ -1,44 +1,45 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
-    '@angular':                   '../lib/@angular',
-    'angular2-in-memory-web-api': '../lib/angular2-in-memory-web-api',
-    'rxjs':                       '../lib/rxjs'
+    'app':                        'pages/angular2/angular2-poc/app', // 'dist',
+    '@angular':                   'pages/angular2/lib/@angular',
+    'rxjs':                       'pages/angular2/lib/rxjs'
   };
+
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+      'app': {main: 'main.js', defaultExtension: 'js'},
+      'rxjs': {defaultExtension: 'js'}
   };
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
+
+  var packageNames = [
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/http',
+      '@angular/platform-browser',
+      '@angular/platform-browser-dynamic',
+      '@angular/router',
+      '@angular/router-deprecated',
+      '@angular/testing',
+      '@angular/upgrade'
   ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
+
+  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+  packageNames.forEach(function (pkgName) {
+      packages[pkgName] = {main: 'index.js', defaultExtension: 'js'};
+  });
+
   var config = {
-    map: map,
-    packages: packages
+      map: map,
+      packages: packages
   };
+
+  // filterSystemConfig - index.html's chance to modify config before we register it.
+  if (global.filterSystemConfig) {
+      global.filterSystemConfig(config);
+  }
+
   System.config(config);
+
 })(this);
