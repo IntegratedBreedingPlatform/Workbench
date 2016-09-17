fbdd66a07bc931c608eee7ce825ba24764bbfaf7
Nahuel Soldevilla <nsoldevilla@droptek.com>
2016-09-17 20:44:48 -0300

    Run Angular 2 test with PhantomJS
    Target es5 when building angular 2 app so Phantom can run the tests
    Add karma execution to pom and karma.conf-ng2.ci to use with maven
    Bump karma phantom launcher version
    
    Issue: BMS-3307 / BMS-3142 / BMS-3316

---
+++ src/main/web/karma.conf-ng2.ci.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var baseConfig = require('./karma.conf-ng2.js');
+
+module.exports = function(config) {
+	baseConfig(config);
+
+	// Override base config
+	config.set({
+		logLevel: config.LOG_DEBUG,
+		singleRun: true,
+		colors: false,
+		autoWatch: false,
+		reporters: ['progress', 'junit', 'coverage'],
+		preprocessors: {
+			'src/appsNg2/**/*.js': ['coverage']
+		},
+		junitReporter: {
+			// These will not be interpreted unless the file name starts with TESTS-,
+			// but right now the parsing is failing (see jira.codehaus.org/browse/SONARJS-304)
+			// so I'm "disabling" them for now
+			outputFile: 'reports/junit/js-ng2-tests-junit.xml'
+		},
+		coverageReporter: {
+			type: 'lcov',
+			dir: 'reports',
+			subdir: 'coverage-ng2'
+		}
+	});
+};
