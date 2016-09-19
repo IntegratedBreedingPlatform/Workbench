b62c001e50f9ff6a40143080c0c9a3498db48462
Vanina Maletta <vmaletta@droptek.com>
2016-09-19 11:56:40 -0300

    Test Role Model

---
+++ src/main/web/src/appsNg2/admin/app/shared/models/role.model.spec.ts
@@ -0,0 +1,22 @@
+import { provide }    from '@angular/core';
+import { addProviders, inject, async, TestBed , ComponentFixture } from "@angular/core/testing";
+import { Role } from './role.model'
+import 'rxjs/add/operator/map';
+
+export function main() {
+
+    describe('Role Model Test', () => {
+
+      function createRole() {
+        return new Role("0", "description");
+      }
+
+      it('should be createable by constructor', () => {
+        let role = createRole();
+        expect(role.id).toBe("0");
+        expect(role.description).toBe("description);
+      });
+
+    });
+
+  }
