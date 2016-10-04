135f6661a67f69c6e2438518e1d784805cf96ced
Vanina <vanina@leafnode.io>
2016-10-04 18:34:19 -0300

    User datagrid test

--- src/main/web/src/appsNg2/admin/app/users/users-datagrid.component.spec.ts
+++ src/main/web/src/appsNg2/admin/app/users/users-datagrid.component.spec.ts
@@ -22,7 +22,8 @@ export function main() {
       let roleService: RoleService
 
       function createArrayOfUsers () {
-          return [ new User("0", "", "", "", "", "", "")
+          return [ new User("0", "Vanina", "Maletta", "vmaletta", "technician", "vanina@leafnode.io", "0"),
+                    new User("1", "Clarysabel", "Tovar", "ctovar", "admin", "clarysabel@leafnode.io", "0")
                  ];
       }
 
@@ -34,6 +35,36 @@ export function main() {
 
       it('should have items', () => {
         expect(grid.table.items).not.toBe(null);
+      });
+
+      it('check usernames', () => {
+        expect (grid.table.items[0].username).toBe('vmaletta');
+        expect (grid.table.items[1].username).toBe('ctovar');
+      });
+
+      it ('Should match the total rows', function() {
+        expect (grid.table.totalRows).toBe(items.length);
+      });
+
+      it ('Should get startRow equals to 0', function () {
+        grid.table.currentPageIndex = 0;
+        expect (grid.table.startRow).toBe(0);
+      });
+
+      it ('Should get items.length equals to 3', function () {
+        expect (grid.table.items.length).toBe(2);
+      });
+
+      it ('Should get items.length equals to 2', function () {
+        grid.table.items = [ new User("0", "Vanina", "Maletta", "vmaletta", "admin", "vanina@leafnode.io", "0"),
+                  new User("1", "Clarysabel", "Tovar", "ctovar", "admin", "clarysabel@leafnode.io", "0")];
+        expect (grid.table.items.length).toBe(2);
+      });
+
+      it ('Should filter by typeToSearch equals to A', function () {
+        grid.table.sortBy = 'role';
+        grid.table.searchValue = new User("1", "Clarysabel", "Tovar", "ctovar", "admin", "clarysabel@leafnode.io", "0");
+        expect (grid.table.itemsFiltered.length).toBe(1);
       });
 
     });
