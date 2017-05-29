7423d983c067ce48d8fd3c83602599ec31f78830
cuenyad <cuenya.diego@gmail.com>
2017-05-29 16:48:06 -0300

    Test fixed.
    ISSUE:BMS-4179
    Reviewer:Nahuel

--- src/test/java/org/generationcp/ibpworkbench/germplasm/containers/ListsForGermplasmQueryTest.java
+++ src/test/java/org/generationcp/ibpworkbench/germplasm/containers/ListsForGermplasmQueryTest.java
@@ -1,25 +1,27 @@
 
 package org.generationcp.ibpworkbench.germplasm.containers;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.vaadin.ui.Link;
+import com.vaadin.data.Item;
+import org.generationcp.ibpworkbench.ui.common.LinkButton;
 import org.generationcp.middleware.data.initializer.GermplasmListTestDataInitializer;
 import org.generationcp.middleware.manager.api.GermplasmListManager;
+import org.generationcp.middleware.pojos.GermplasmList;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import com.vaadin.data.Item;
+import java.util.Arrays;
+import java.util.List;
 
-import junit.framework.Assert;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class ListsForGermplasmQueryTest {
 
 	private ListsForGermplasmQuery listForGermplasmQuery;
 	private GermplasmListTestDataInitializer germplasmListTestDataInitializer;
+	private GermplasmList germplasmList;
 
 	@Before
 	public void setUp() {
@@ -27,26 +29,28 @@ public class ListsForGermplasmQueryTest {
 		final GermplasmListManager germplasmListManager = Mockito.mock(GermplasmListManager.class);
 		Mockito.when(germplasmListManager.countGermplasmListByGIDandProgramUUID(Matchers.anyInt(), Matchers.anyString()))
 				.thenReturn((long) 1);
-		Mockito.when(germplasmListManager.getGermplasmListByGIDandProgramUUID(Matchers.anyInt(), Matchers.anyInt(), Matchers.anyInt(),
-				Matchers.anyString())).thenReturn(Arrays.asList(this.germplasmListTestDataInitializer.createGermplasmList(1)));
+		germplasmList = this.germplasmListTestDataInitializer.createGermplasmList(1);
+		Mockito.when(germplasmListManager
+			.getGermplasmListByGIDandProgramUUID(Matchers.anyInt(), Matchers.anyInt(), Matchers.anyInt(), Matchers.anyString()))
+			.thenReturn(Arrays.asList(germplasmList));
 		this.listForGermplasmQuery = new ListsForGermplasmQuery(germplasmListManager, 1, "1");
 	}
 
 	@Test
 	public void testSize() {
-		Assert.assertEquals("The size should be 1", (long) 1, this.listForGermplasmQuery.size());
+		assertThat(this.listForGermplasmQuery.size(),equalTo(1));
 	}
 
 	@Test
 	public void testLoadItems() {
 		final List<Item> items = this.listForGermplasmQuery.loadItems(1, 1);
 		final Item item = items.get(0);
-		Assert.assertEquals("The id should be 1", "1", item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_ID).toString());
-		Assert.assertEquals("The id should be List 1 Description", "List 1 Description",
-				item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_DESCRIPTION).toString());
-		Assert.assertEquals("The id should be List 1", "List 1",
-			((Link) item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_NAME).getValue()).getCaption());
-		Assert.assertEquals("The date should be 20150101", "20150101",
-				item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_DATE).toString());
+		final String url = "/BreedingManager/main/list-manager?restartApplication&lists=" + germplasmList.getId();
+
+		assertThat(germplasmList.getId().toString(),equalTo(item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_ID).toString()));
+		assertThat(germplasmList.getDescription(),equalTo(item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_DESCRIPTION).toString()));
+		assertThat(germplasmList.getName(),equalTo(((LinkButton) item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_NAME).getValue()).getCaption()));
+		assertThat(germplasmList.getDate().toString(),equalTo(item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_DATE).toString()));
+		assertThat(url, equalTo(((LinkButton) item.getItemProperty(ListsForGermplasmQuery.GERMPLASMLIST_NAME).getValue()).getResource().getURL().toString()));
 	}
 }
