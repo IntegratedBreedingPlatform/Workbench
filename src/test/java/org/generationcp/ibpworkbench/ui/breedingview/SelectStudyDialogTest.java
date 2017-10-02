271e84ec668ecdd37d7bd2650423a84548e67232
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-10-02 15:58:12 +0800

    Updated the test method and added assertions
    BMS-2237

--- src/test/java/org/generationcp/ibpworkbench/ui/breedingview/SelectStudyDialogTest.java
+++ src/test/java/org/generationcp/ibpworkbench/ui/breedingview/SelectStudyDialogTest.java
@@ -4,6 +4,9 @@ package org.generationcp.ibpworkbench.ui.breedingview;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Window;
 
+import junit.framework.Assert;
+
+import org.generationcp.middleware.data.initializer.StudyReferenceTestDataInitializer;
 import org.generationcp.middleware.domain.oms.StudyType;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.StudyDataManager;
@@ -14,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.AssertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SelectStudyDialogTest {
@@ -36,7 +40,13 @@ public class SelectStudyDialogTest {
 	@Test
 	public void testCreateStudyTreeTable() throws MiddlewareQueryException {
 		Mockito.when(currentProject.getUniqueID()).thenReturn(UNIQUE_ID);
-		dialog.createStudyTreeTable();
-		Mockito.verify(studyDataManager, Mockito.times(1)).getRootFolders(UNIQUE_ID, StudyType.nurseriesAndTrials());
+		Mockito.when(this.studyDataManager.getRootFolders(this.currentProject.getUniqueID(), StudyType.trials())).thenReturn(StudyReferenceTestDataInitializer.createStudyReferenceList(5));
+		BreedingViewTreeTable table = dialog.createStudyTreeTable();
+		Mockito.verify(studyDataManager, Mockito.times(1)).getRootFolders(UNIQUE_ID, StudyType.trials());
+		Assert.assertEquals("There should be 33 property ids.", 3, table.getContainerPropertyIds().size());
+		Assert.assertTrue("The property ids should contain " + SelectStudyDialog.STUDY_NAME, table.getContainerPropertyIds().contains(SelectStudyDialog.STUDY_NAME));
+		Assert.assertTrue("The property ids should contain " + SelectStudyDialog.OBJECTIVE, table.getContainerPropertyIds().contains(SelectStudyDialog.OBJECTIVE));
+		Assert.assertTrue("The property ids should contain " + SelectStudyDialog.TITLE, table.getContainerPropertyIds().contains(SelectStudyDialog.TITLE));
+		Assert.assertEquals("The table should contain 5 studies", 5, table.getNodeMap().size());
 	}
 }
