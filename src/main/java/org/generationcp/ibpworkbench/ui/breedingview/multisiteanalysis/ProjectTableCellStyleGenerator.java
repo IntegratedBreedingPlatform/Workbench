ac34a4784235f4e4b3f5c82a77c1d601ec979964
Aldrin Batac <aldrin@leafnode.io>
2018-01-29 17:13:32 +0800

    Fix the error in loading Workbench if there's no previously selected program yet.
    BMS-4607

--- src/main/java/org/generationcp/ibpworkbench/ui/breedingview/multisiteanalysis/ProjectTableCellStyleGenerator.java
+++ src/main/java/org/generationcp/ibpworkbench/ui/breedingview/multisiteanalysis/ProjectTableCellStyleGenerator.java
@@ -2,6 +2,7 @@ package org.generationcp.ibpworkbench.ui.breedingview.multisiteanalysis;
 
 import com.vaadin.ui.Table.CellStyleGenerator;
 import org.generationcp.commons.spring.util.ContextUtil;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.pojos.workbench.Project;
 import org.springframework.beans.factory.annotation.Configurable;
 
@@ -23,7 +24,15 @@ public class ProjectTableCellStyleGenerator implements CellStyleGenerator {
 	public String getStyle(final Object itemId, final Object propertyId) {
 		final Object projectId = itemId;
 
-		final Project currentProject = contextUtil.getProjectInContext();
+		Project currentProject = null;
+
+		try {
+			currentProject = contextUtil.getProjectInContext();
+		} catch (final MiddlewareQueryException e) {
+			// This error is expected if there's no previously selected program yet
+			// for the current user. For this case, just ignore the error.
+		}
+
 
 		if (projectId == null) {
 			return "project-table";
