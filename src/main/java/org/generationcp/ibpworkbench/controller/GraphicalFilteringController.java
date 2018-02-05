f32ffa8e749e756f0a55de78940c9a7bcd4af2ad
Nahuel Soldevilla <nahuel@leafnode.io>
2018-02-05 17:17:37 +0100

    Include graphical filtering controller
    Prototype Proof-of-concept for testing
    
    Issue: BMS-4691
    Reviewer: Clary

---
+++ src/main/java/org/generationcp/ibpworkbench/controller/GraphicalFilteringController.java
@@ -0,0 +1,17 @@
+package org.generationcp.ibpworkbench.controller;
+
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/graphical-filtering")
+public class GraphicalFilteringController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String angular2poc(Model model) throws MiddlewareQueryException {
+		return "BrAPI-Graphical-Filtering/index";
+	}
+}
