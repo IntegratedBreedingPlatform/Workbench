876ff1243d5740f6427469176441943971d7a887
Darla Hermo <darla@leafnode.io>
2018-02-14 18:53:09 +0800

    Removed final modifiers inserted from code cleanup as it's causing mocks in test class not to be inserted and hence test failures
    BMS-2870

--- src/main/java/org/generationcp/ibpworkbench/actions/RunMultiSiteAction.java
+++ src/main/java/org/generationcp/ibpworkbench/actions/RunMultiSiteAction.java
@@ -67,9 +67,9 @@ public class RunMultiSiteAction implements ClickListener {
 
 	private IBPWorkbenchApplication workbenchApplication;
 
-	private final MultiSiteDataExporter multiSiteDataExporter = new MultiSiteDataExporter();
+	private MultiSiteDataExporter multiSiteDataExporter = new MultiSiteDataExporter();
 
-	private final InstallationDirectoryUtil installationDirectoryUtil = new InstallationDirectoryUtil();
+	private InstallationDirectoryUtil installationDirectoryUtil = new InstallationDirectoryUtil();
 
 	private MultiSiteParameters multiSiteParameters;
 
@@ -77,7 +77,7 @@ public class RunMultiSiteAction implements ClickListener {
 
 	private Table selectTraitsTable;
 
-	private final ZipUtil zipUtil = new ZipUtil();
+	private ZipUtil zipUtil = new ZipUtil();
 
 	public RunMultiSiteAction() {
 		// for unit testing
