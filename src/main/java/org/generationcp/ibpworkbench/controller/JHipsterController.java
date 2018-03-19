7d35185f6a111ba3cd901c6e5c6da3283f7462fc
Nahuel Soldevilla <nahuel@leafnode.io>
2018-03-19 16:58:05 -0300

    Make crop name available in the front-end
    Issue: BMS-4754
    Reviewer: None

--- src/main/java/org/generationcp/ibpworkbench/controller/JHipsterController.java
+++ src/main/java/org/generationcp/ibpworkbench/controller/JHipsterController.java
@@ -1,14 +1,26 @@
 package org.generationcp.ibpworkbench.controller;
 
+import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/jhipster")
 public class JHipsterController {
+
+	@Resource
+	protected ContextUtil contextUtil;
+
+	@ModelAttribute("cropName")
+	public String getCropName() {
+		return this.contextUtil.getProjectInContext().getCropType().getCropName();
+	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(Model model) throws MiddlewareQueryException {
