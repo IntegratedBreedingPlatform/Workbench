b6cc35d48be67fdf08ba6c549d9366f388466b91
Darla Hermo <darla@leafnode.io>
2018-06-13 15:45:03 +0800

    Adjust Site Admin / Manage Users module to store Role as an object and not just a string
    BMS-4932

--- src/main/web/src/appsNg2/admin/app/shared/models/user.model.ts
+++ src/main/web/src/appsNg2/admin/app/shared/models/user.model.ts
@@ -1,10 +1,12 @@
+import { Role } from './../shared/models/role.model';
+
 export class User {
 
     constructor(public id: string,
                 public firstName: string,
                 public lastName: string,
                 public username: string,
-                public role: string,
+                public role: Role,
                 public email: string,
                 public status: string ) {
     }
