b0a03c97d44a2d330363b80a42a5bbcf990507d9
cuenyad <cuenya.diego@gmail.com>
2018-07-17 18:37:50 -0300

    Adding new modal to import plate file csv.
    Issue: BMS-5011
    Reviewer: None

---
+++ src/main/jhipster/src/main/webapp/app/entities/sample/sample-import-plate.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'jhi-sample-import-plate',
+    templateUrl: './sample-import-plate.component.html',
+    styleUrls: ['./sample-import-plate.component.css']
+})
+
+export class SampleImportPlateComponent {
+}
