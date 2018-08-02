81d7bf948645266b58e4e3df6b0289ce95843c32
cuenyad <cuenya.diego@gmail.com>
2018-08-02 15:09:28 -0300

    Refactoring the html/js
    ISSUE BMS-5036/BMS-5034
    Review none

--- src/main/web/src/apps/ontology/app-components/add-formula-view/addFormulaView.js
+++ src/main/web/src/apps/ontology/app-components/add-formula-view/addFormulaView.js
@@ -2,40 +2,33 @@
 'use strict';
 
 (function () {
-	var app = angular.module('addFormula', ['multiSelect', 'input', 'textArea', 'select', 'variables', 'properties', 'variableTypes',
-		'methods', 'scales', 'variableState', 'utilities', 'range', 'errorList']);
+	var app = angular.module('addFormula', ['input', 'textArea', 'variables', 'variableState', 'utilities', 'errorList']);
 
-	app.controller('AddFormulaController', ['$scope', '$window', '$location', 'variablesService', 'variableTypesService',
-		'propertiesService', 'methodsService', 'scalesService', 'variableStateService', 'serviceUtilities', 'formUtilities',
-
-		function ($scope, $window, $location, variablesService, variableTypesService, propertiesService, methodsService, scalesService,
+	app.controller('AddFormulaController', ['$scope', '$window', '$location', 'variablesService', 'variableStateService' ,'serviceUtilities', 'formUtilities',
+		function ($scope, $window, $location, variablesService,
 				  variableStateService, serviceUtilities, formUtilities) {
 
 			var VARIABLES_PATH = '/variables/', storedData;
 			$scope.serverErrors = {};
 			storedData = variableStateService.getVariableState();
-
-			$scope.variable = storedData.variable;
-			$scope.serverErrors.general = storedData.errors;
-			$scope.variable.formula = creatingFormula($scope.variable);
-			// Clear the variable state service now that we are back on the variables screen
 			variableStateService.reset();
 
+			$scope.model = storedData.variable;
+			$scope.serverErrors.general = storedData.errors;
 
-
-			$scope.saveFormula = function (e, formula) {
+			$scope.saveFormula = function (e, variable) {
 				e.preventDefault();
 
-				if ($scope.avForm.$valid) {
+				if ($scope.afForm.$valid) {
 					$scope.submitted = true;
 					$scope.serverErrors = {};
 
-					variablesService.addFormula(formula).then(function () {
+					variablesService.addFormula(variable.formula).then(function () {
 						variableStateService.reset();
-						$location.path(VARIABLES_PATH+$scope.variable.id);
-						variablesService.deleteVariablesFromCache([parseInt($scope.variable.id)]);
+						$location.path(VARIABLES_PATH + variable.id);
+						variablesService.deleteVariablesFromCache([parseInt(variable.id)]);
 					}, function (response) {
-						$scope.avForm.$setUntouched();
+						$scope.afForm.$setUntouched();
 						$scope.serverErrors = serviceUtilities.serverErrorHandler($scope.serverErrors, response);
 						$scope.submitted = false;
 					});
@@ -45,25 +38,11 @@
 			$scope.cancel = function (e) {
 				e.preventDefault();
 				variableStateService.reset();
-				$location.path(VARIABLES_PATH+$scope.variable.id);
+				$location.path(VARIABLES_PATH + $scope.model.id);
 
 			};
 
-			$scope.formGroupClass = formUtilities.formGroupClassGenerator($scope, 'avForm');
-
-			function creatingFormula(variable) {
-				var formula = {
-					"definition": "",
-					"target":  {
-						id: variable.id
-					},
-					"description": "",
-					"name": "",
-					"active": true,
-					"formulaId": 0
-				};
-				return formula;
-			};
+			$scope.formGroupClass = formUtilities.formGroupClassGenerator($scope, 'afForm');
 		}
 	]);
 }());
