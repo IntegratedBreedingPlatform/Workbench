79fe194041323813f28bdd819010ae6f8c0d3570
Aldrin Batac <aldrin.batac@leafnode.io>
2018-12-13 09:55:39 +0800

    Address Sonar violations.
    IBP-1897

--- src/main/java/org/generationcp/ibpworkbench/germplasm/pedigree/GraphVizUtility.java
+++ src/main/java/org/generationcp/ibpworkbench/germplasm/pedigree/GraphVizUtility.java
@@ -1,32 +1,28 @@
 
 package org.generationcp.ibpworkbench.germplasm.pedigree;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-
+import com.vaadin.ui.Window;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
-
-import com.vaadin.ui.Window;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 @Configurable
 public class GraphVizUtility {
 
-	private static String TEMP_DIR = System.getProperty("user.dir");
+	public static final String START_GRAPH = "strict digraph G {";
+	public static final String END_GRAPH = "}";
+
+	private static final String TEMP_DIR = System.getProperty("user.dir");
 	private static final Logger LOG = LoggerFactory.getLogger(GraphVizUtility.class);
 
 	private static final String BSLASH = "\\";
 	private static final String FSLASH = "/";
-
 
 	@Value("${graphviz.executable.path}")
 	private String grapthvizExecutablePath;
@@ -44,26 +40,13 @@ public class GraphVizUtility {
 	private String imageOutputPath = null;
 
 	/**
-	 * Constructor: creates a new GraphViz object that will contain a graph.
-	 *
-	 * @throws URISyntaxException
-	 * @throws FileNotFoundException
-	 */
-	public GraphVizUtility() throws URISyntaxException, FileNotFoundException {
-	}
-
-	/**
 	 * Initialize this GraphVizUtility instance.
-	 *
+	 * <p>
 	 * This method should set the path of GraphViz dot executable.
 	 */
 	public void initialize() {
 		final File dotFile = new File(grapthvizExecutablePath).getAbsoluteFile();
 		this.dotPath = dotFile.getAbsolutePath();
-	}
-
-	public String getImageOutputPath() {
-		return this.imageOutputPath;
 	}
 
 	public void setImageOutputPath(final String imageOutputPath) {
@@ -94,17 +77,10 @@ public class GraphVizUtility {
 	}
 
 	/**
-	 * Adds a newline to the graph's source.
-	 */
-	public void addln() {
-		this.graph.append('\n');
-	}
-
-	/**
 	 * Returns the graph as an image in binary format.
 	 *
 	 * @param dotSource Source of the graph to be drawn.
-	 * @param type Type of the output image to be produced, e.g.: gif, dot, fig, pdf, ps, svg, png.
+	 * @param type      Type of the output image to be produced, e.g.: gif, dot, fig, pdf, ps, svg, png.
 	 * @return A byte array containing the image of the graph.
 	 */
 	public byte[] getGraph(final String dotSource, final String type) {
@@ -131,26 +107,14 @@ public class GraphVizUtility {
 	 * Writes the graph's image in a file.
 	 *
 	 * @param img A byte array containing the image of the graph.
-	 * @param file Name of the file to where we want to write.
-	 * @return Success: 1, Failure: -1
-	 */
-	public int writeGraphToFile(final byte[] img, final String file) {
-		final File to = new File(file);
-		return this.writeGraphToFile(img, to);
-	}
-
-	/**
-	 * Writes the graph's image in a file.
-	 *
-	 * @param img A byte array containing the image of the graph.
-	 * @param to A File object to where we want to write.
+	 * @param to  A File object to where we want to write.
 	 * @return Success: 1, Failure: -1
 	 */
 	public int writeGraphToFile(final byte[] img, final File to) {
 		try {
-			final FileOutputStream fos = new FileOutputStream(to);
-			fos.write(img);
-			fos.close();
+			try (FileOutputStream fos = new FileOutputStream(to)) {
+				fos.write(img);
+			}
 		} catch (final java.io.IOException ioe) {
 			GraphVizUtility.LOG.error(ioe.getMessage(), ioe);
 			return -1;
@@ -161,7 +125,7 @@ public class GraphVizUtility {
 	/**
 	 * It will call the external dot program, and return the image in binary format.
 	 *
-	 * @param dot Source of the graph (in dot language).
+	 * @param dot  Source of the graph (in dot language).
 	 * @param type Type of the output image to be produced, e.g.: gif, dot, fig, pdf, ps, svg, png.
 	 * @return The image of the graph in .gif format.
 	 */
@@ -176,12 +140,9 @@ public class GraphVizUtility {
 			final Process p = rt.exec(args);
 			p.waitFor();
 
-			final FileInputStream in = new FileInputStream(img.getAbsolutePath());
-			imgStream = new byte[in.available()];
-			in.read(imgStream);
-			// Close it if we need to
-			if (in != null) {
-				in.close();
+			try (FileInputStream in = new FileInputStream(img.getAbsolutePath())) {
+				imgStream = new byte[in.available()];
+				in.read(imgStream);
 			}
 
 			if (!img.delete()) {
@@ -189,7 +150,7 @@ public class GraphVizUtility {
 			}
 		} catch (final java.io.IOException ioe) {
 			GraphVizUtility.LOG.error(
-					"Error: In I/O processing of tempfile in dir " + GraphVizUtility.TEMP_DIR + "\n or in calling external command", ioe);
+				"Error: In I/O processing of tempfile in dir " + GraphVizUtility.TEMP_DIR + "\n or in calling external command", ioe);
 		} catch (final InterruptedException ie) {
 			GraphVizUtility.LOG.error("Error: the execution of the external program was interrupted", ie);
 		}
@@ -207,9 +168,9 @@ public class GraphVizUtility {
 		File temp;
 		try {
 			temp = File.createTempFile("graph_", ".dot.tmp", new File(GraphVizUtility.TEMP_DIR));
-			final FileWriter fout = new FileWriter(temp);
-			fout.write(str);
-			fout.close();
+			try (FileWriter fout = new FileWriter(temp)) {
+				fout.write(str);
+			}
 		} catch (final Exception e) {
 			GraphVizUtility.LOG.error("Error: I/O error while writing the dot source to temp file!", e);
 			return null;
@@ -217,54 +178,13 @@ public class GraphVizUtility {
 		return temp;
 	}
 
-	/**
-	 * Returns a string that is used to start a graph.
-	 *
-	 * @return A string to open a graph.
-	 */
-	public String startGraph() {
-		return "strict digraph G {";
-	}
-
-	/**
-	 * Returns a string that is used to end a graph.
-	 *
-	 * @return A string to close a graph.
-	 */
-	public String endGraph() {
-		return "}";
-	}
-
-	/**
-	 * Read a DOT graph from a text file.
-	 *
-	 * @param input Input text file containing the DOT graph source.
-	 */
-	public void readSource(final String input) {
-		final StringBuilder sb = new StringBuilder();
-
-		try {
-			final FileInputStream fis = new FileInputStream(input);
-			final DataInputStream dis = new DataInputStream(fis);
-			final BufferedReader br = new BufferedReader(new InputStreamReader(dis));
-			String line;
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-			dis.close();
-		} catch (final Exception e) {
-			GraphVizUtility.LOG.error("Error: " + e.getMessage(), e);
-		}
-
-		this.graph = sb;
-	}
-
 	public static String createImageOutputPathForWindow(final Window window) {
-		return window.getWindow().getApplication().getContext().getBaseDirectory().getAbsolutePath().replace(GraphVizUtility.BSLASH,
-				GraphVizUtility.FSLASH) + "/WEB-INF/image";
+		return window.getWindow().getApplication().getContext().getBaseDirectory().getAbsolutePath().replace(
+			GraphVizUtility.BSLASH,
+			GraphVizUtility.FSLASH) + "/WEB-INF/image";
 	}
 
-	public String graphVizOutputPath(final String fileName) throws URISyntaxException {
+	public String graphVizOutputPath(final String fileName) {
 		return this.imageOutputPath + File.separator + fileName;
 	}
 
