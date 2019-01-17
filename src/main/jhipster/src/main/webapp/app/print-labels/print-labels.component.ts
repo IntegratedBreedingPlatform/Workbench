21908bebce76582d3a11edaf833d1d0168be724a
Nahuel Soldevilla <nahuel@leafnode.io>
2019-01-17 17:39:00 -0300

    Add basic print label markup with mocked label columns
    Issue: IBP-2078 / IBP-2302
    Reviewer: None

--- src/main/jhipster/src/main/webapp/app/print-labels/print-labels.component.ts
+++ src/main/jhipster/src/main/webapp/app/print-labels/print-labels.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserLabelPrinting } from './user-label-printing.model';
+import { JhiLanguageService } from 'ng-jhipster';
+import { printMockData } from './print-labels.mock-data';
 
 @Component({
     selector: 'jhi-print-labels',
     templateUrl: './print-labels.component.html',
-    styles: []
+    styleUrls: ['./print-labels.component.css']
 })
 export class PrintLabelsComponent implements OnInit {
     private datasetId: number;
     private studyId: number;
+    private userLabelPrinting: UserLabelPrinting = {};
+    private printMockData = printMockData;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,
+                private languageService: JhiLanguageService) {
     }
 
     ngOnInit() {
---
+++ src/main/jhipster/src/main/webapp/app/print-labels/print-labels.mock-data.ts
@@ -0,0 +1,33 @@
+export const printMockData = {
+    labelTypes: [{
+        title: 'Study Details',
+        fields: [{
+            id: 8131,
+            name: 'NREP'
+        }, {
+            id: 8180,
+            name: 'LOCATION_NAME'
+        }, {
+            id: 8263,
+            name: 'NPSEL'
+        }, {
+            id: 18,
+            name: 'Scale'
+        }]
+    }, {
+        title: 'Study Details',
+        fields: [{
+            id: 8131,
+            name: 'NREP'
+        }, {
+            id: 8180,
+            name: 'LOCATION_NAME'
+        }, {
+            id: 8263,
+            name: 'NPSEL'
+        }, {
+            id: 18,
+            name: 'Scale'
+        }]
+    }]
+};
