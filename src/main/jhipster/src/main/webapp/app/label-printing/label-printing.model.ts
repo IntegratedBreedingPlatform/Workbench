0f9607357d0af9e4a002334486026b5082ffabd7
cuenyad <cuenya.diego@gmail.com>
2019-02-22 18:25:49 -0300

    Adding toolId
    Issue: IBP-2375
    Reviewer: None

--- src/main/jhipster/src/main/webapp/app/label-printing/label-printing.model.ts
+++ src/main/jhipster/src/main/webapp/app/label-printing/label-printing.model.ts
@@ -71,6 +71,7 @@ export class PresetSetting {
     constructor(
         public  id?: number,
         public  programUUID?: string,
+        public  toolId?: number,
         public  toolSection?: string,
         public  name?: string,
         public  type?: string,
