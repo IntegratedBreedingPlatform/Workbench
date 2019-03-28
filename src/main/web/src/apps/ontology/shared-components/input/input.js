1c8945aafd208e4c89ffe6a6cec372b4b490410c
cuenyad <cuenya.diego@gmail.com>
2019-03-28 12:43:55 -0300

    Adding a new parameter to allow disabled the input.
    IBP-2574

--- src/main/web/src/apps/ontology/shared-components/input/input.js
+++ src/main/web/src/apps/ontology/shared-components/input/input.js
@@ -8,7 +8,9 @@
 		return {
 			controller: ['$scope', function($scope) {
 				$scope.editable = editable($scope);
-
+				$scope.isDisabled = function(){
+					return $scope.inputDisabled;
+				};
 				// We cannot assign values to one time binding scope properties that are not defined
 				// on the directive instance, so instead we must use a different scope property
 				// and just read from the initial property as to whether the value was given or not.
