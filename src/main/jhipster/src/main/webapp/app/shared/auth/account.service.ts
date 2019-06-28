bce0bdae4064acc0d49d929f66c0252dd9131e2a
Nahuel Soldevilla <nahuel@leafnode.io>
2019-06-28 16:02:28 -0300

    Send crop and program to account service
    Issue: IBP-2792 / IBP-1435 / IBP-2691
    Reviewer: None

--- src/main/jhipster/src/main/webapp/app/shared/auth/account.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/auth/account.service.ts
@@ -3,11 +3,19 @@ import { Observable } from 'rxjs';
 import { SERVER_API_URL } from '../../app.constants';
 import { HttpClient } from '@angular/common/http';
 
+declare const cropName: string;
+declare const currentProgramId: string;
+
 @Injectable()
 export class AccountService  {
     constructor(private http: HttpClient) { }
 
     get(): Observable<Account> {
-        return this.http.get<Account>(SERVER_API_URL + '/account');
+        return this.http.get<Account>(SERVER_API_URL + '/account', {
+            params: {
+                cropName,
+                programUuid: currentProgramId
+            }
+        });
     }
 }
