b50851ac06cda205689cd745f11f6ebd6a42f3a1
Nahuel Soldevilla <nahuel@leafnode.io>
2019-07-23 17:58:08 -0300

    Role creation form mockup
    - Permission tree selector mockup
    - Validation on some fields
    - Get role types
    
    Issue: IBP-2792 / IBP-1192 / IBP-2912
    Reviewer: None

--- src/main/web/src/appsNg2/admin/app/roles/role-card.component.ts
+++ src/main/web/src/appsNg2/admin/app/roles/role-card.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Role } from '../shared/models/role.model';
+import { RoleType } from '../shared/models/role-type.model';
+import { RoleService } from '../shared/services/role.service';
 
 @Component({
-    selector: 'roles-creation',
+    selector: 'role-card',
     templateUrl: './role-card.component.html',
     moduleId: module.id
 })
@@ -12,17 +15,27 @@ export class RoleCardComponent implements OnInit {
     isEditing: boolean;
     isVisible = true;
 
-    constructor(private route: ActivatedRoute, private router: Router) {
+    model: Role = new Role();
+    // TODO merge into Role
+    roleType: RoleType;
+    roleTypes: RoleType[];
+
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private roleService: RoleService) {
     }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             this.isEditing = params['isEditing'];
         });
+        this.roleService.getRoleTypes().subscribe((roleTypes) => {
+            this.roleTypes = roleTypes;
+        })
     }
 
     isFormValid(form: NgForm) {
-        return false;
+        return form.valid;
     }
 
     cancel(form: NgForm) {
@@ -34,6 +47,10 @@ export class RoleCardComponent implements OnInit {
     }
 
     editRole(form: NgForm) {
+
+    }
+
+    changeRoleType() {
 
     }
 }
--- src/main/web/src/appsNg2/admin/app/shared/models/role.model.ts
+++ src/main/web/src/appsNg2/admin/app/shared/models/role.model.ts
@@ -1,11 +1,11 @@
 export class Role {
 
-    constructor(public id: number,
-                public name: string,
-                public description: string,
-                public type: string,
-                public active: boolean,
-                public editable: boolean,
-                public assignable: boolean) {
+    constructor(public id?: number,
+                public name?: string,
+                public description?: string,
+                public type?: string,
+                public active?: boolean,
+                public editable?: boolean,
+                public assignable?: boolean) {
     }
 }
