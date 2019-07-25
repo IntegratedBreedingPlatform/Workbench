577fb0e66678aaeb21cab629440e7ab75317842b
cuenyad <cuenya.diego@gmail.com>
2019-07-25 01:01:30 -0300

    adding Permission list to Role.
    Issue:IBP-1194
    Review: none

--- src/main/web/src/appsNg2/admin/app/shared/models/role.model.ts
+++ src/main/web/src/appsNg2/admin/app/shared/models/role.model.ts
@@ -1,11 +1,14 @@
+import { Permission } from './permission.model';
+
 export class Role {
 
     constructor(public id?: number,
                 public name?: string,
                 public description?: string,
                 public type?: string,
-                public active?: boolean,
-                public editable?: boolean,
-                public assignable?: boolean) {
+                public active?: string,
+                public editable?: string,
+                public assignable?: string,
+                public permissions?: Permission[]) {
     }
 }
