ae4d12273c39f9e0068af34c010ee9ef1a1aa27f
Nahuel Soldevilla <nahuel@leafnode.io>
2019-09-02 15:52:49 -0300

    Make username validation consistent with email
    Use email local-part validation to validate username.
    
    Issue: IBP-2981
    Reviewer: None

--- src/main/web/src/appsNg2/admin/app/shared/validators/email-validator.component.ts
+++ src/main/web/src/appsNg2/admin/app/shared/validators/email-validator.component.ts
@@ -2,6 +2,8 @@ import { Directive } from '@angular/core';
 import { NG_VALIDATORS } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 
+export const EMAIL_LOCAL_PART_REGEX = '^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*';
+
 @Directive({
   selector: '[validateEmail][ngModel]',
   providers: [
@@ -11,7 +13,7 @@ import { FormControl } from '@angular/forms';
 export class EmailValidator { }
 
 function validateEmail(c: FormControl) {
-  let EMAIL_REGEXP = /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/i;
+  let EMAIL_REGEXP = new RegExp(EMAIL_LOCAL_PART_REGEX + "@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$" , "i");
 
   return EMAIL_REGEXP.test(c.value) ? null : {
     validateEmail: {
