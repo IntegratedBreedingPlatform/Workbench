49c35cf589dc582147bea71540df752921d5fca9
cuenyad <cuenya.diego@gmail.com>
2019-12-09 16:02:30 -0300

    New method to recover parameters from the url
    ISSUE: IBP-3188

--- src/main/web/src/js/login.js
+++ src/main/web/src/js/login.js
@@ -355,4 +355,13 @@
 				});
 		}
 	});
+
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
+
 }());
