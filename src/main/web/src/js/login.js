4544c54e3d186a07386639157aa14721ca2b7205
cuenyad <cuenya.diego@gmail.com>
2019-12-10 16:20:47 -0300

    Remove function and adding get-url-parameter.js
    IBP-3188
    Review none

--- src/main/web/src/js/login.js
+++ src/main/web/src/js/login.js
@@ -372,11 +372,4 @@
 		}
 	});
 
-	function getUrlParameter(name) {
-		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-		var results = regex.exec(location);// FIXME Improvement to search the parameter in the whole URL. problem-related to recover the parameter after the Hash.
-		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-	};
-
 }());
