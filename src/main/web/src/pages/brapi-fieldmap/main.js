a14a2b183360f5c036854ffd330b08cdd6de0db5
Nahuel Soldevilla <nahuel@leafnode.io>
2020-04-06 15:21:23 -0300

    geojson editor progress update
    - More responsive layout
    - Show load button for creation
    
    Issue: IBP-3183 / IBP-3218

--- src/main/web/src/pages/brapi-fieldmap/main.js
+++ src/main/web/src/pages/brapi-fieldmap/main.js
@@ -3,14 +3,22 @@ TODO Move to jhipster folder
  - use webpack gulp integration to copy libs (see gulp/tasks/lib.js)
  */
 
-var instanceId = getUrlParameter('instanceId');
-var cropName = getUrlParameter('cropName');
-const brapi_endpoint = '/bmsapi/' + cropName + '/brapi/v2';
+const instanceId = getUrlParameter('instanceId'),
+	cropName = getUrlParameter('cropName'),
+	hasLayout = getUrlParameter('hasLayout') === "true",
+	brapi_endpoint = '/bmsapi/' + cropName + '/brapi/v2';
 
-var fieldMap = new BrAPIFieldmap("#map", brapi_endpoint, {
+const fieldMap = new BrAPIFieldmap("#map", brapi_endpoint, {
 	brapi_auth: JSON.parse(localStorage['bms.xAuthToken']).token
 });
+
 fieldMap.setLocation(instanceId);
+
+if (hasLayout) {
+	load();
+} else {
+	$('#load').show();
+}
 
 // Functions
 
