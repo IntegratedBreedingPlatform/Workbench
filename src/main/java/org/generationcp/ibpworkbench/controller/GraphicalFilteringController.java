6ebccf81f8d38774e4bd2528571546478fea94a8
Gelli Samson <gelli@leafnode.io>
2020-04-13 17:49:46 +0800

    Add attribute default page size in graphical-filtering controller and in workbench.properties Add html attribute for the defaultPageSize Modify script to use defaultPageSize value IBP-3248-BrApiMaxPageSize

--- src/main/java/org/generationcp/ibpworkbench/controller/GraphicalFilteringController.java
+++ src/main/java/org/generationcp/ibpworkbench/controller/GraphicalFilteringController.java
@@ -1,8 +1,10 @@
 package org.generationcp.ibpworkbench.controller;
 
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +13,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class GraphicalFilteringController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(Model model) throws MiddlewareQueryException {
+	public String index(final Model model) throws MiddlewareQueryException {
 		return "BrAPI-Graphical-Filtering/index";
+	}
+
+	@Value("${pagedresult.default.page.size}")
+	public String defaultPageSize;
+
+	@ModelAttribute("defaultPageSize")
+	public String getDefaultPageSize() {
+		return this.defaultPageSize;
 	}
 }
