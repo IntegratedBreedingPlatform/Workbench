ba294d67db8a854b29fa26b1c6e1d27ef72dece8
Gelli Samson <gelli@leafnode.io>
2020-04-20 16:06:43 +0800

    Fix Failing unit test by adding mtype value. IBP-3616-SetMprgnValue

--- src/test/java/org/generationcp/ibpworkbench/data/initializer/MethodViewTestDataInitializer.java
+++ src/test/java/org/generationcp/ibpworkbench/data/initializer/MethodViewTestDataInitializer.java
@@ -1,12 +1,14 @@
 package org.generationcp.ibpworkbench.data.initializer;
 
 import org.generationcp.ibpworkbench.ui.programmethods.MethodView;
+import org.generationcp.middleware.pojos.workbench.MethodType;
 
 public class MethodViewTestDataInitializer {
 	public static MethodView createMethodView() {
 		final MethodView methodView = new MethodView();
 		methodView.setMname("method name");
 		methodView.setMcode("Method Code");
+		methodView.setMtype(MethodType.GENERATIVE.getCode());
 		return methodView;
 	}
 	public static MethodView createMethodView(final String type) {
