d99fe28898adcab3aef1a50c64c3fbc96820ce82
Aldrin Batac <aldrin.batac@leafnode.io>
2020-10-02 15:20:35 +0800

    Use Bootstrap 4 css file only for this component to match with ng-bootstrap widgets. I didn't update the bootstrap library included in index.html to avoid breaking other modules (sample manager, lot-creation dialog ...)
    IBP-4071

--- src/main/jhipster/src/main/webapp/app/germplasm-manager/germplasm-tab.component.ts
+++ src/main/jhipster/src/main/webapp/app/germplasm-manager/germplasm-tab.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ParamContext } from '../shared/service/param.context';
+import { ViewEncapsulation } from '@angular/core';
 
 
 @Component({
+    encapsulation: ViewEncapsulation.None,
     selector: 'jhi-germplasm-tab',
     templateUrl: './germplasm-tab.component.html',
-    styles: []
+    styleUrls: ['../../../../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class GermplasmTabComponent implements OnInit {
 
