f996425cd6fdd3068c919447423ba17308d1e284
Nahuel Soldevilla <nahuel@leafnode.io>
2020-11-19 18:03:32 -0300

    Add numberOfItemsToShow to listPreview
    Issue: IBP-4098

--- src/main/jhipster/src/main/webapp/app/shared/util/list-preview.ts
+++ src/main/jhipster/src/main/webapp/app/shared/util/list-preview.ts
@@ -2,13 +2,13 @@
  * Similar to server-side Util.buildErrorMessageFromList
  * @param list
  */
-export function listPreview(list: (string | number)[]) {
+export function listPreview(list: (string | number)[], numberOfItemsToShow = 3) {
     if (!list || !list.length) {
         return '';
     }
-    let preview = list.slice(0, 3).join(', ');
-    if (list.length > 3) {
-        preview += ' and ' + (list.length - 3) + ' more';
+    let preview = list.slice(0, numberOfItemsToShow).join(', ');
+    if (list.length > numberOfItemsToShow) {
+        preview += ' and ' + (list.length - numberOfItemsToShow) + ' more';
     }
     return preview;
 }
