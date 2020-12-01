8d5b48aeec722d19c94d087a8b2958d3e21b10c2
Nahuel Soldevilla <nahuel@leafnode.io>
2020-12-01 17:49:18 -0300

    Merge isFormValid() into canProceed()
    Fixes scenario with mixed data, e.g: hasAllBreedingMethods but not all
    locations.
    
    Issue: IBP-4098

--- src/main/jhipster/src/main/webapp/app/germplasm-manager/import/germplasm-import-basic-details.component.ts
+++ src/main/jhipster/src/main/webapp/app/germplasm-manager/import/germplasm-import-basic-details.component.ts
@@ -158,15 +158,10 @@ export class GermplasmImportBasicDetailsComponent implements OnInit {
     }
 
     canProceed() {
-        return this.isFormValid() || (
-            this.hasAllBasicDetails()
-                && this.unmapped.length === 0
-        );
-    }
-
-    private isFormValid() {
         // TODO complete
-        return this.breedingMethodSelected && this.locationSelected;
+        return (this.breedingMethodSelected || this.hasAllBreedingMethods())
+            && (this.locationSelected || this.hasAllLocations())
+            && this.unmapped.length === 0
     }
 
     dragStart($event, code) {
