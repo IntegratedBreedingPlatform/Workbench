1f34406a110982409705780f1ca752bb26488b66
Darla Grace Hermo <darla@leafnode.io>
2020-12-03 15:52:33 +0800

    Re-use GermplasmNameTypeModel
    IBP-4150

--- src/main/jhipster/src/main/webapp/app/shared/name-type/model/name-type.model.ts
+++
@@ -1,9 +0,0 @@
-export class NameType {
-    constructor(
-        public  code?: string,
-        private id?: number,
-        private name?: string,
-        private value?: string
-    ) {
-    }
-}
