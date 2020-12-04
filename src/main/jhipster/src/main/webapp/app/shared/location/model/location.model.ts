c66426481b344baec7c29e2a95707a3c51e15cbf
cuenyad <cuenya.diego@gmail.com>
2020-12-04 16:21:18 -0300

    Adding Breeding Location type
    IBP-4098
    Review none

--- src/main/jhipster/src/main/webapp/app/shared/location/model/location.model.ts
+++ src/main/jhipster/src/main/webapp/app/shared/location/model/location.model.ts
@@ -14,5 +14,6 @@ export class LocationModel {
 export enum LocationTypeEnum {
     COUNTRY = 405,
     PROVINCE = 406,
-    BREEDING_LOCATION = 410
+    BREEDING_LOCATION = 410,
+    SEED_STORAGE_LOCATION = 1500
 }
