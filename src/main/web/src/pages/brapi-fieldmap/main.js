5783ad41e46aa98cdef491bf7a5ce734276f39f5
Aldrin Batac <aldrin.batac@leafnode.io>
2020-12-07 08:53:52 +0800

    Convert fieldmap tool to AngularJS.
    IBP-3632

--- src/main/web/src/pages/brapi-fieldmap/main.js
+++ src/main/web/src/pages/brapi-fieldmap/main.js
@@ -3,40 +3,101 @@ TODO Move to jhipster folder
  - use webpack gulp integration to copy libs (see gulp/tasks/lib.js)
  */
 
-const instanceId = getUrlParameter('instanceId'),
-	cropName = getUrlParameter('cropName'),
-	hasLayout = getUrlParameter('hasLayout') === "true",
-	brapi_endpoint = '/bmsapi/' + cropName + '/brapi/v2';
+var mainApp = angular.module('mainApp', ['ui.bootstrap']);
 
-const fieldMap = new BrAPIFieldmap("#map", brapi_endpoint, {
-	brapi_auth: JSON.parse(localStorage['bms.xAuthToken']).token
-});
+mainApp.controller('MainController', ['$scope', '$uibModal', '$http', function ($scope, $uibModal, $http) {
 
-fieldMap.setLocation(instanceId);
+	const instanceId = getUrlParameter('instanceId'),
+		cropName = getUrlParameter('cropName'),
+		hasLayout = getUrlParameter('hasLayout') === "true",
+		brapi_endpoint = '/bmsapi/' + cropName + '/brapi/v2';
 
-if (hasLayout) {
-	load();
-} else {
-	$('#load').show();
-}
+	const fieldMap = new BrAPIFieldmap("#map", brapi_endpoint, {
+		brapi_auth: JSON.parse(localStorage['bms.xAuthToken']).token
+	});
 
-// Functions
+	$scope.editMode = false;
+	$scope.length = '';
+	$scope.width = '';
 
-function load() {
-	fieldMap.opts.plotLength = d3.select('#length').node().value;
-	fieldMap.opts.plotWidth = d3.select('#width').node().value;
-	fieldMap.load(instanceId);
-}
+	$scope.init = function () {
+		fieldMap.setLocation(instanceId);
+		if (hasLayout) {
+			$scope.load();
+		} else {
+			$scope.editMode = true;
+		}
+	};
 
-function update() {
-	// TODO bootstrap modal
-	if (hasLayout) {
-		const proceed = confirm("You are going to override the existing layout");
-		if (!proceed) return;
+
+	$scope.load = function () {
+		fieldMap.opts.plotLength = $scope.length;
+		fieldMap.opts.plotWidth = $scope.width;
+		fieldMap.load(instanceId);
 	}
 
-	fieldMap.update().then(
-		// TODO toast
-		(resp) => alert(resp),
-		(resp) => alert(resp));
-}
+	$scope.update = function () {
+		if (hasLayout) {
+			var modalInstance = $scope.openConfirmModal();
+			modalInstance.result.then((isOK) => {
+				if (isOK) {
+					fieldMap.update().then(
+						// TODO toast
+						(resp) => alert(resp),
+						(resp) => alert(resp));
+				}
+			});
+		} else {
+			fieldMap.update().then(
+				// TODO toast
+				(resp) => alert(resp),
+				(resp) => alert(resp));
+		}
+	};
+
+	$scope.openConfirmModal = function (message) {
+		var modalInstance = $uibModal.open({
+			animation: true,
+			template: '<div class="modal-body">\n' +
+				'    <div class="row form-group">\n' +
+				'        <div class="col-xs-11 col-md-11">\n' +
+				'            <label class="modal-title fbk-modal-title">Confirmation</label>\n' +
+				'        </div>\n' +
+				'        <div class="col-xs-1 col-md-1">\n' +
+				'            <button class="close pull-right glyphicon glyphicon-remove" ng-click="cancel()"/>\n' +
+				'        </div>\n' +
+				'    </div>\n' +
+				'    <div class="row">\n' +
+				'        <div class="col-xs-12 col-md-12">\n' +
+				'            <label class="control-label" ng-bind-html="text"></label>\n' +
+				'        </div>\n' +
+				'    </div>\n' +
+				'</div>\n' +
+				'<div class="modal-footer">\n' +
+				'    <button class="btn btn-default" ng-click="cancel()" ng-bind="cancelButtonLabel">Cancel</button>\n' +
+				'    <button class="btn btn-primary" ng-click="confirm()" ng-bind="confirmButtonLabel">OK</button>\n' +
+				'</div>',
+			controller: function ($scope, $uibModalInstance) {
+				$scope.text = message;
+				$scope.confirmButtonLabel = confirmButtonLabel || okLabel;
+				$scope.cancelButtonLabel = cancelButtonLabel || cancelLabel;
+
+				$scope.confirm = function () {
+					$uibModalInstance.close(true);
+				};
+
+				$scope.cancel = function () {
+					$uibModalInstance.close(false);
+				};
+			}
+		});
+
+		return modalInstance;
+	}
+
+
+	$scope.init();
+
+}]);
+
+
