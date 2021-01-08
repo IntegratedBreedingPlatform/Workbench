cf9ba64efc2da05753f6673beeee57d1129fa9cc
Nahuel Soldevilla <nahuel@leafnode.io>
2021-01-08 18:32:16 -0300

    "My Programs" style and pagination
    Issue: IBP-2824

--- src/main/jhipster/src/main/webapp/app/entities/program/program.component.ts
+++ src/main/jhipster/src/main/webapp/app/entities/program/program.component.ts
@@ -1,25 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ProgramService } from '../../shared/program/service/program.service';
 import { Observable } from 'rxjs';
 import { Program } from '../../shared/program/model/program';
+import { HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
+    encapsulation: ViewEncapsulation.None,
     selector: 'jhi-program',
     templateUrl: './program.component.html',
     // TODO migrate IBP-4093
-    styleUrls: ['../../../content/css/global-bs4.scss']
+    styleUrls: [
+        '../../../content/css/global-bs4.scss',
+        './program.scss'
+    ]
 })
 export class ProgramComponent {
 
-    programs: Observable<Program[]>;
+    programs: Program[];
+    itemCount: any;
+    pageSize = 20;
+    page = 1;
+    isLoading = false;
 
     constructor(
         private programService: ProgramService
     ) {
-        this.programs = this.programService.getPrograms();
+        this.loadPage();
     }
 
     onProgramSelect(program: Program) {
-        window.parent.postMessage({programSelected: program}, '*');
+        window.parent.postMessage({ programSelected: program }, '*');
+    }
+
+    isSelected(program: Program) {
+        return program.programUUID === localStorage['programUUID']
+    }
+
+    loadPage() {
+        this.isLoading = true;
+        this.programService.getPrograms({
+            page: this.page - 1,
+            size: this.pageSize
+        }).pipe(
+            finalize(() => this.isLoading = false)
+        ).subscribe((resp: HttpResponse<Program[]>) => {
+            this.programs = resp.body;
+            this.itemCount = resp.headers.get('X-Total-Count');
+        });
     }
 }
--- src/main/jhipster/src/main/webapp/app/shared/program/service/program.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/program/service/program.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { SERVER_API_URL } from '../../../app.constants';
 import { Program } from '../model/program';
 import { Observable } from 'rxjs';
+import { createRequestOption } from '../..';
 
 @Injectable()
 export class ProgramService {
@@ -10,7 +11,10 @@ export class ProgramService {
     constructor(private http: HttpClient) {
     }
 
-    getPrograms(): Observable<Program[]> {
-        return this.http.get<Program[]>(SERVER_API_URL + 'programs');
+    getPrograms(pageable): Observable<HttpResponse<Program[]>> {
+        return this.http.get<Program[]>(SERVER_API_URL + 'programs', {
+            params: createRequestOption(pageable),
+            observe: 'response'
+        });
     }
 }
