51ff7df72c52198dceca6b5c54f058db82766e4b
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-02-02 20:28:29 +0800

    Added logic to check if derivative line
    IBP-4199

--- src/main/java/org/generationcp/ibpworkbench/germplasm/pedigree/CreatePedigreeGraph.java
+++ src/main/java/org/generationcp/ibpworkbench/germplasm/pedigree/CreatePedigreeGraph.java
@@ -4,13 +4,16 @@ package org.generationcp.ibpworkbench.germplasm.pedigree;
 import com.vaadin.ui.Window;
 import org.generationcp.ibpworkbench.germplasm.GermplasmQueries;
 import org.generationcp.middleware.manager.api.GermplasmDataManager;
+import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.GermplasmPedigreeTree;
 import org.generationcp.middleware.pojos.GermplasmPedigreeTreeNode;
 import org.generationcp.middleware.pojos.Method;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -27,6 +30,7 @@ public class CreatePedigreeGraph {
 	private boolean showBreedingMethod;
 	private Set<Integer> breedingMethodIds;
 	private Map<Integer, Method> breedingMethodMap;
+	private Map<Integer, Germplasm> immediateSourceMap;
 
 	public CreatePedigreeGraph(
 		final int gid, final int level, final Boolean includeDerivativeLines, final boolean showBreedingMethod, final Window window,
@@ -39,6 +43,7 @@ public class CreatePedigreeGraph {
 		this.includeDerivativeLines = includeDerivativeLines;
 		this.showBreedingMethod = showBreedingMethod;
 		this.breedingMethodIds = new HashSet<>();
+		immediateSourceMap = new HashMap<>();
 	}
 
 	public CreatePedigreeGraph(final int gid, final int level, final Window window, final GermplasmQueries qQuery,
@@ -87,6 +92,7 @@ public class CreatePedigreeGraph {
 	private void createDiGraphNode() {
 		final GermplasmPedigreeTree germplasmPedigreeTree =
 			this.qQuery.generatePedigreeTree(Integer.valueOf(this.gid), this.level, this.includeDerivativeLines);
+		this.populateImmediateSourceMap(germplasmPedigreeTree.getRoot());
 		if(this.showBreedingMethod) {
 			this.getBreedingMethodIds(germplasmPedigreeTree.getRoot());
 			this.breedingMethodMap = this.germplasmDataManager.getMethodsByIDs(new ArrayList<>(this.breedingMethodIds)).stream()
@@ -98,6 +104,20 @@ public class CreatePedigreeGraph {
 			this.graphVizUtility.addln(leafNodeGIDRoot + ";");
 		} else {
 			this.addNode(germplasmPedigreeTree.getRoot(), 1);
+		}
+	}
+
+	void populateImmediateSourceMap(GermplasmPedigreeTreeNode root) {
+		final List<Integer> immediateSourceGidList = new ArrayList<>();
+		this.getImmediateSourceGidList(root, immediateSourceGidList);
+		this.immediateSourceMap = this.germplasmDataManager.getGermplasms(immediateSourceGidList).stream()
+			.collect(Collectors.toMap(Germplasm::getGid, Function.identity()));
+	}
+
+	void getImmediateSourceGidList(final GermplasmPedigreeTreeNode node, final List<Integer> immediateSourceGidList) {
+		immediateSourceGidList.add(node.getGermplasm().getGpid2());
+		for (final GermplasmPedigreeTreeNode parent : node.getLinkedNodes()) {
+			this.getImmediateSourceGidList(parent, immediateSourceGidList);
 		}
 	}
 
@@ -141,7 +161,8 @@ public class CreatePedigreeGraph {
 
 		for (final GermplasmPedigreeTreeNode parent : node.getLinkedNodes()) {
 			final boolean isFemaleParent = parent.getGermplasm().getGid().equals(node.getGermplasm().getGpid1());
-			final boolean isDerivative = node.getGermplasm().getGpid1().equals(node.getGermplasm().getGpid2());
+			final boolean isDerivative = node.getGermplasm().getGpid1().equals(node.getGermplasm().getGpid2()) ||
+				node.getGermplasm().getGpid1().equals(this.immediateSourceMap.get(node.getGermplasm().getGpid2()).getGpid1());
 			final String leafNodeGID = this.createNodeTextWithFormatting(parent);
 			final String parentNodeGID = this.createNodeTextWithFormatting(node);
 
