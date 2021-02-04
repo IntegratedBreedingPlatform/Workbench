0b9c845c72b983e10e584e8b7e5ab394ac7a1633
gellisamson <gelli@leafnode.io>
2021-02-04 12:16:30 +0800

    GenerateFileNameController IBP-4198-ExportFileName

---
+++ src/main/java/org/generationcp/ibpworkbench/controller/GenerateFileNameController.java
@@ -0,0 +1,21 @@
+package org.generationcp.ibpworkbench.controller;
+
+import org.generationcp.commons.util.FileNameGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(GenerateFileNameController.URL)
+public class GenerateFileNameController {
+
+	public static final String URL = "/fileNameGenerator";
+
+	@RequestMapping(value = "/generateFileName/{fileType}")
+	@ResponseBody
+	public String getExportFileName(@PathVariable final String fileType, @RequestParam final String fileName) {
+		return FileNameGenerator.generateFileName(fileName, fileType);
+	}
+}
