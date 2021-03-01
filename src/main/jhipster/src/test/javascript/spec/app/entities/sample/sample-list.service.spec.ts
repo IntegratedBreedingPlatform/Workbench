e5c86ab6ba6a05fd2d698d9749703283e700b5e3
Gelli Samson <gelli@leafnode.io>
2021-03-01 08:30:22 +0800

    Fix unit test IBP-4317-SamplListService

--- src/main/jhipster/src/test/javascript/spec/app/entities/sample/sample-list.service.spec.ts
+++ src/main/jhipster/src/test/javascript/spec/app/entities/sample/sample-list.service.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { SERVER_API_URL } from '../../../../../../main/webapp/app/app.constants';
 import {SampleListService} from '../../../../../../main/webapp/app/entities/sample/sample-list.service';
 import {SampleList} from '../../../../../../main/webapp/app/entities/sample/sample-list.model';
+import {ParamContext} from "../../../../../../main/webapp/app/shared/service/param.context";
 
 declare const cropName: string;
 declare const currentProgramId: string;
@@ -21,6 +22,7 @@ describe('Service Tests', () => {
                     HttpClientTestingModule
                 ],
                 providers: [
+                    ParamContext,
                     SampleListService
                 ]
 
