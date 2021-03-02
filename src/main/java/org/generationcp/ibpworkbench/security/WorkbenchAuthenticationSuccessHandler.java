c677200bfa983c056126e23f1110be9c9679fdfc
Leonel Kovacic <leonel@leafnode.io>
2021-03-02 19:26:38 -0300

    IBP-4324: code review changes

--- src/main/java/org/generationcp/ibpworkbench/security/WorkbenchAuthenticationSuccessHandler.java
+++ src/main/java/org/generationcp/ibpworkbench/security/WorkbenchAuthenticationSuccessHandler.java
@@ -30,7 +30,6 @@ public class WorkbenchAuthenticationSuccessHandler implements AuthenticationSucc
 
 	private static final Logger LOG = LoggerFactory.getLogger(WorkbenchAuthenticationSuccessHandler.class);
 
-	//TODO: fix me! For some reason there is a 404 using "/main". We need to check why servlet url mapping doesn't work for
 	private static final String DEFAULT_TARGET_URL = "/main/";
 
 	@Autowired
