b376bd6af0493ab64cc0abac5d607e67c4fff22f
Aldrin Batac <aldrin.batac@leafnode.io>
2021-03-09 13:54:15 +0800

    Add UI for Attributes.
    IBP-4052 IBP-4414

--- src/main/jhipster/src/main/webapp/app/germplasm-details/attributes/attributes-pane.component.ts
+++ src/main/jhipster/src/main/webapp/app/germplasm-details/attributes/attributes-pane.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { JhiLanguageService } from 'ng-jhipster';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'jhi-attributes-pane',
     templateUrl: './attributes-pane.component.html'
 })
 export class AttributesPaneComponent implements OnInit {
+
+    constructor(public languageservice: JhiLanguageService,
+                public translateService: TranslateService) {
+    }
+
     ngOnInit(): void {
     }
 
