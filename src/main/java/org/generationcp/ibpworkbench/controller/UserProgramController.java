aa8857384339297f21f327bc45ea6f813faa0965
Gelli Samson <gelli@leafnode.io>
2021-03-25 13:01:24 +0800

    PR Cleanup: Change endpoint to userProgramController/setUserProgramInfo Added program last opened date and user's last opened project Reload authorities by project IBP-4421-SetContextSelectedProgram

---
+++ src/main/java/org/generationcp/ibpworkbench/controller/UserProgramController.java
@@ -0,0 +1,68 @@
+package org.generationcp.ibpworkbench.controller;
+
+import org.generationcp.commons.security.AuthorizationService;
+import org.generationcp.commons.spring.util.ContextUtil;
+import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import org.generationcp.middleware.pojos.workbench.Project;
+import org.generationcp.middleware.pojos.workbench.ProjectUserInfo;
+import org.generationcp.middleware.service.api.user.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Controller
+@RequestMapping(UserProgramController.URL)
+public class UserProgramController {
+    public static final String URL = "/userProgramController/";
+
+    @Resource
+    private ContextUtil contextUtil;
+
+    @Resource
+    private WorkbenchDataManager workbenchDataManager;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private AuthorizationService authorizationService;
+
+    @Resource
+    private HttpServletRequest request;
+
+    @RequestMapping(value = "/setUserProgramInfo", method = RequestMethod.POST)
+    public ResponseEntity<String> setUserProgramInfo(@RequestBody final String programUUID) {
+        final Project project = this.workbenchDataManager.getProjectByUuid(programUUID);
+        if (project != null) {
+            org.generationcp.commons.util.ContextUtil.setContextInfo(this.request,
+                    this.contextUtil.getCurrentWorkbenchUserId(), project.getProjectId(), null);
+
+            this.updateProjectLastOpenedDate(project);
+
+            this.authorizationService.reloadAuthorities(project);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Updates Project last Opened Date and User's last opened Project
+     * @param project - Opened / Selected project
+     */
+    private void updateProjectLastOpenedDate(final Project project) {
+        ProjectUserInfo pui = this.userService.getProjectUserInfoByProjectIdAndUserId(project.getProjectId(), this.contextUtil.getCurrentWorkbenchUserId());
+        if (pui == null) {
+            pui = new ProjectUserInfo(project, this.contextUtil.getCurrentWorkbenchUser());
+        }
+        pui.setLastOpenDate(new Date());
+        this.userService.saveOrUpdateProjectUserInfo(pui);
+
+        project.setLastOpenDate(new Date());
+        this.workbenchDataManager.saveOrUpdateProject(project);
+    }
+}
+
--- src/main/jhipster/src/main/webapp/app/app.constants.ts
+++ src/main/jhipster/src/main/webapp/app/app.constants.ts
@@ -9,7 +9,7 @@ export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
 export const GERMPLASM_BROWSER_DEFAULT_URL = '/ibpworkbench/maingpsb/germplasm-';
 export const BREEDING_METHODS_BROWSER_DEFAULT_URL = '/ibpworkbench/content/ProgramMethods';
 export const MAX_PAGE_SIZE = process.env.MAX_PAGE_SIZE;
-export const SELECT_PROGRAM_URL = '/ibpworkbench/controller/selectProgram/setCurrentProgram';
+export const SELECT_PROGRAM_URL = '/ibpworkbench/controller/userProgramController/setUserProgramInfo';
 
 // HELP
 export const HELP_BASE_URL = '/ibpworkbench/controller/help/getUrl/';
