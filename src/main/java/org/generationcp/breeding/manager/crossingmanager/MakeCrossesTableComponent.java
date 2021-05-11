3eca925f3d7591c1ee45b68e2967ed846e5786a8
Leonel Kovacic <leonel@leafnode.io>
2021-05-11 11:18:00 -0300

    IBP-4563: cleanup

--- src/main/java/org/generationcp/breeding/manager/crossingmanager/MakeCrossesTableComponent.java
+++ src/main/java/org/generationcp/breeding/manager/crossingmanager/MakeCrossesTableComponent.java
@@ -53,7 +53,6 @@ import org.generationcp.breeding.manager.util.BreedingManagerUtil;
 import org.generationcp.commons.parsing.pojo.ImportedCross;
 import org.generationcp.commons.parsing.pojo.ImportedGermplasmParent;
 import org.generationcp.commons.ruleengine.generator.SeedSourceGenerator;
-import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.commons.util.CollectionTransformationUtil;
 import org.generationcp.commons.util.DateUtil;
 import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
@@ -165,9 +164,6 @@ public class MakeCrossesTableComponent extends VerticalLayout
 
 	@Autowired
 	private StudyDataManager studyDataManager;
-
-	@Resource
-	private ContextUtil contextUtil;
 
 	private Label lblReviewCrosses;
 
