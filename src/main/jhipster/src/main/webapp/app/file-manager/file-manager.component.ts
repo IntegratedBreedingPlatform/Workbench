39788453a33cdd31549ff58202678ae3fd751dd3
Nahuel Soldevilla <nahuel@leafnode.io>
2021-05-11 20:00:45 -0300

    IBP-4568: Modal header fileName, cosmetic changes
    - don't auto open input file
    - close modal

--- src/main/jhipster/src/main/webapp/app/file-manager/file-manager.component.ts
+++ src/main/jhipster/src/main/webapp/app/file-manager/file-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'jhi-file-manager',
@@ -7,11 +8,22 @@ import { ActivatedRoute, Route } from '@angular/router';
 })
 export class FileManagerComponent {
     fileKey: string;
+    fileName: string;
 
     constructor(
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private activeModal: NgbActiveModal,
     ) {
         const routeParams = this.route.snapshot.paramMap;
+        const queryParamMap = this.route.snapshot.queryParamMap;
         this.fileKey = routeParams.get('fileKey');
+        this.fileName = queryParamMap.get('fileName');
+    }
+
+    cancel() {
+        this.activeModal.dismiss();
+        if ((<any>window.parent).closeModal) {
+            (<any>window.parent).closeModal();
+        }
     }
 }
