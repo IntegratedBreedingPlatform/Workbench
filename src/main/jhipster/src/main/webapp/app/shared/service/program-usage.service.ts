5aba09bf7e355b3d928e4df4671db6675affdac4
cuenyad <cuenya.diego@gmail.com>
2021-05-13 15:15:53 -0300

    Adding setSelectedProgram removed in this Branch.
    Issue: IBP-4397
    Review: none

--- src/main/jhipster/src/main/webapp/app/shared/service/program-usage.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/service/program-usage.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { SERVER_API_URL } from '../../app.constants';
+import { SERVER_API_URL, USER_PROGRAM_INFO } from '../../app.constants';
 import { Observable } from 'rxjs';
 import { Program } from '../program/model/program';
 
@@ -17,8 +17,12 @@ export class ProgramUsageService {
     }
 
     save(cropName: any, programUUID: any): Observable<any> {
-        return this.http.post<any>(SERVER_API_URL + `crops/${cropName}/program-usage?programUUID=${programUUID}`,null, {
+        return this.http.post<any>(SERVER_API_URL + `crops/${cropName}/program-usage?programUUID=${programUUID}`, null, {
             observe: 'response'
         });
+    }
+
+    setSelectedProgram(programUUID: string): Observable<any> {
+        return this.http.post(USER_PROGRAM_INFO, programUUID, { responseType: 'text' });
     }
 }
