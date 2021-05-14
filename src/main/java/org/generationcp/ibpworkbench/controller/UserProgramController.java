a5b25e617f8b949d53012aa2196aeef0bb08ee98
cuenyad <cuenya.diego@gmail.com>
2021-05-14 07:55:59 -0300

    Remove TODO
    Issue: IBP-4397
    Review: none

--- src/main/java/org/generationcp/ibpworkbench/controller/UserProgramController.java
+++ src/main/java/org/generationcp/ibpworkbench/controller/UserProgramController.java
@@ -4,16 +4,15 @@ import org.generationcp.commons.security.AuthorizationService;
 import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
 import org.generationcp.middleware.pojos.workbench.Project;
-import org.generationcp.middleware.pojos.workbench.ProjectUserInfo;
-import org.generationcp.middleware.service.api.user.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 
 @Controller
 @RequestMapping(UserProgramController.URL)
@@ -33,10 +32,10 @@ public class UserProgramController {
     @Resource
     private HttpServletRequest request;
 
-    //TODO:  this controller was implemented in IBP-4421 fixing an authentication issue.
-    //    After that in IBP-4397 was implemented the APIs to return the last project user selected and save the project selected by the user.
-    //    Also was implement ParamContext in AccountService and RouteAccessService
-    //    to set the crop and programUUID and recover the user authorities to validate the access.
+      /* This controller was implemented in IBP-4421 fixing an authentication issue.
+         As a part of the IBP-4397 was implemented the APIs to return the last project user selected and save the project selected by the user.
+         Also was implement ParamContext in AccountService and RouteAccessService
+         to set the crop and programUUID and recover the user authorities to validate the access. */
 
     @RequestMapping(value = "/userProgramInfo", method = RequestMethod.POST)
     public ResponseEntity<String> setUserProgramInfo(@RequestBody final String programUUID) {
