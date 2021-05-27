8ec60b0c608b1be5363be955e08d811e5483c759
cuenyad <cuenya.diego@gmail.com>
2021-05-27 12:04:05 -0300

    Removing cropName and programId getting from ParamContext.
    IBP-4669
    Review none

--- src/main/jhipster/src/main/webapp/app/label-printing/label-printing.context.ts
+++ src/main/jhipster/src/main/webapp/app/label-printing/label-printing.context.ts
@@ -3,8 +3,6 @@ import { LabelPrintingType } from './label-printing.component';
 
 @Injectable()
 export class LabelPrintingContext {
-    programId: string;
-    cropName: string;
     studyId: number;
     datasetId: number;
     printingLabelType: LabelPrintingType;
