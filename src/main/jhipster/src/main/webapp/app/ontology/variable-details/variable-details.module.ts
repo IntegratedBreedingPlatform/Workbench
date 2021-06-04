8bef6f439025f1ce5dc26e2b060013a8fc067f0c
Leonel Kovacic <leonel@leafnode.io>
2021-06-04 11:26:05 -0300

    IBP-4609: removed unused components

--- src/main/jhipster/src/main/webapp/app/ontology/variable-details/variable-details.module.ts
+++ src/main/jhipster/src/main/webapp/app/ontology/variable-details/variable-details.module.ts
@@ -3,7 +3,6 @@ import { BmsjHipsterSharedModule } from '../../shared';
 import { RouterModule } from '@angular/router';
 import { VariableDetailsComponent } from './variable-details.component';
 import { variableDetailsRoutes } from './variable-details.route';
-import { VariableDetailsModalComponent, VariableDetailsPopupComponent } from './variable-details-modal.component';
 import { DetailsComponent } from './details.component';
 import { ValidValuesComponent } from './valid-values.component';
 import { VariableDetailsContext } from './variable-details.context';
@@ -15,15 +14,11 @@ import { VariableDetailsContext } from './variable-details.context';
     ],
     declarations: [
         VariableDetailsComponent,
-        VariableDetailsPopupComponent,
-        VariableDetailsModalComponent,
         DetailsComponent,
         ValidValuesComponent
     ],
     entryComponents: [
         VariableDetailsComponent,
-        VariableDetailsPopupComponent,
-        VariableDetailsModalComponent,
         DetailsComponent,
         ValidValuesComponent
     ],
