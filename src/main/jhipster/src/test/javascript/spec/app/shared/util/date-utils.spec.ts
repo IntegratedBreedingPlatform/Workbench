8f6f6ea6a940d89c760fbdf10fde480e789e77e2
Nahuel Soldevilla <nahuel@leafnode.io>
2021-06-29 16:26:35 -0300

    IBP-4440: moment.js dependency and date-utils.ts (format, validate)
    Modified from c550d7c574a4b0e499d15a2b3e86801adca9cad3

---
+++ src/main/jhipster/src/test/javascript/spec/app/shared/util/date-utils.spec.ts
@@ -0,0 +1,21 @@
+import { DateFormatEnum, formatDate, isValidDate } from '../../../../../../main/webapp/app/shared/util/date-utils';
+
+describe('date-utils', () => {
+    it('should format dates', () => {
+        expect(formatDate('20200515', DateFormatEnum.ISO_8601)).toBe('2020-05-15')
+        expect(formatDate('20200515', DateFormatEnum.ISO_8601_AND_TIME)).toBe('2020-05-15 00:00:00')
+        expect(formatDate('2020-05-15 10:00:00', DateFormatEnum.ISO_8601_AND_TIME)).toBe('2020-05-15 10:00:00')
+        expect(formatDate('2020-05-15 10:00:00', DateFormatEnum.ISO_8601)).toBe('2020-05-15')
+        expect(formatDate('2020-05-15 10:00:00', DateFormatEnum.ISO_8601_NUMBER)).toBe('20200515')
+    })
+
+    it('should validate dates', () => {
+        expect(isValidDate('20200515', DateFormatEnum.ISO_8601)).toBe(false)
+        expect(isValidDate('20200515', DateFormatEnum.ISO_8601_NUMBER)).toBe(true)
+        expect(isValidDate('2020-05-15 10:00:00', DateFormatEnum.ISO_8601)).toBe(false)
+        expect(isValidDate('2020-05-15 10:00:00', DateFormatEnum.ISO_8601_AND_TIME)).toBe(true)
+        expect(isValidDate('2020-05-15', DateFormatEnum.ISO_8601)).toBe(true)
+        expect(isValidDate('2020-05-15', DateFormatEnum.ISO_8601_NUMBER)).toBe(false)
+    })
+
+});
