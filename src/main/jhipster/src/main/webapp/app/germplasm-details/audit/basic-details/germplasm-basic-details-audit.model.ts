67c646b8c66ecdc40ca541485616cc147fe35d04
Leonel Kovacic <leonel@leafnode.io>
2021-07-05 16:32:30 -0300

    IBP-4746: added germplasm group to germplasm basic details audit changes

--- src/main/jhipster/src/main/webapp/app/germplasm-details/audit/basic-details/germplasm-basic-details-audit.model.ts
+++ src/main/jhipster/src/main/webapp/app/germplasm-details/audit/basic-details/germplasm-basic-details-audit.model.ts
@@ -9,8 +9,10 @@ export class GermplasmBasicDetailsAudit extends GermplasmAudit {
         public modifiedDate: number,
         public creationDate: string,
         public locationName: string,
+        public groupId: number,
         public locationChanged: boolean,
         public creationDateChanged: boolean,
+        public groupIdChanged: boolean
     ) {
         super(revisionType, createdBy, createdDate, modifiedBy, modifiedDate);
     }
