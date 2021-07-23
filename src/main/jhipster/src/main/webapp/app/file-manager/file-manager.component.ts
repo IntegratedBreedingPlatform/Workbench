45ff14858168e6e5fe0b9ae57ff0fec3e90c4086
Nahuel Soldevilla <nahuel@leafnode.io>
2021-07-23 17:06:16 -0300

    IBP-4815: Implement file upload in file-manager. Cosmetic changes

--- src/main/jhipster/src/main/webapp/app/file-manager/file-manager.component.ts
+++ src/main/jhipster/src/main/webapp/app/file-manager/file-manager.component.ts
@@ -4,34 +4,64 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ParamContext } from '../shared/service/param.context';
 import { FileService } from '../shared/file/service/file.service';
 import { FileMetadata } from '../shared/file/model/file-metadata';
+import { readAsDataURL } from '../shared/util/file-utils';
+import { HttpErrorResponse } from '@angular/common/http';
+import { formatErrorList } from '../shared/alert/format-error-list';
+import { AlertService } from '../shared/alert/alert.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-file-manager',
-    templateUrl: './file-manager.component.html'
+    templateUrl: './file-manager.component.html',
+    styleUrls: ['./file-manager.scss']
 })
 export class FileManagerComponent {
 
     @ViewChild('fileUpload')
     fileUpload: ElementRef;
+    file: File;
 
     fileMetadata: FileMetadata
-    fileName: string;
+    imgToUploadUrlPreview;
+
+    datasetId: number;
+    observationUnitUUID: string;
+    observationId: number;
+    termId: number;
+
+    isLoading = false;
+    isSaving = false;
+    // TODO
+    acceptedFileTypes = [];
 
     constructor(
         private route: ActivatedRoute,
         private activeModal: NgbActiveModal,
         public context: ParamContext,
-        private fileService: FileService
+        private fileService: FileService,
+        private alertService: AlertService
     ) {
         this.context.readParams();
-        const routeParams = this.route.snapshot.paramMap;
         const queryParamMap = this.route.snapshot.queryParamMap;
-        this.fileService.listFileMetadata(queryParamMap.get('observationId'))
-            .subscribe((fileMetadataList) => {
-                if (fileMetadataList && fileMetadataList.length) {
-                    this.fileMetadata = fileMetadataList[0];
-                }
-            });
+        this.datasetId = Number(queryParamMap.get('datasetId'));
+        this.observationUnitUUID = queryParamMap.get('observationUnitUUID');
+        this.observationId = Number(queryParamMap.get('observationId'));
+        this.termId = Number(queryParamMap.get('termId'));
+
+        if (this.observationId) {
+            this.isLoading = true;
+            this.fileService.listFileMetadata(this.observationId)
+                .pipe(finalize(() => this.isLoading = false))
+                .subscribe((fileMetadataList) => {
+                    if (fileMetadataList && fileMetadataList.length) {
+                        this.fileMetadata = fileMetadataList[0];
+                    }
+                }, (error) => this.onError(error));
+        }
+    }
+
+    isImage(fileName) {
+        return fileName.match(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)
     }
 
     cancel() {
@@ -50,12 +80,47 @@ export class FileManagerComponent {
     }
 
     download() {
-
     }
 
-    onFileChange(evt: any) {
-        const target = evt.target;
-        this.fileName = target.files[0].name;
-        this.fileUpload.nativeElement.innerText = target.files[0].name;
+    async onFileChange(evt: any) {
+        this.file = evt.target.files[0];
+        this.fileUpload.nativeElement.innerText = this.file.name;
+        if (this.isImage(this.file.name)) {
+            this.imgToUploadUrlPreview = await readAsDataURL(this.file);
+        }
+    }
+
+    upload() {
+        this.isSaving = true;
+        // upload file / save observation
+        this.fileService.upload(
+            this.file,
+            this.observationUnitUUID,
+            this.termId
+        ).pipe(
+            finalize(() => this.isSaving = false)
+        ).subscribe(
+            (fileMetadata) => {
+                this.fileMetadata = fileMetadata;
+                this.onSuccess()
+            },
+            (error) => this.onError(error)
+        );
+    }
+
+    private onSuccess() {
+        this.alertService.success('fileManager.upload.success');
+        if (window.parent) {
+            window.parent.postMessage('observations-changed', '*');
+        }
+    }
+
+    private onError(response: HttpErrorResponse) {
+        const msg = formatErrorList(response.error.errors);
+        if (msg) {
+            this.alertService.error('error.custom', { param: msg });
+        } else {
+            this.alertService.error('error.general', null, null);
+        }
     }
 }
--- src/main/jhipster/src/main/webapp/app/shared/file/service/file.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/file/service/file.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { FileMetadata } from '../model/file-metadata';
 import { SERVER_API_URL } from '../../../app.constants';
 import { ParamContext } from '../../service/param.context';
@@ -17,7 +17,24 @@ export class FileService {
     listFileMetadata(observationId): Observable<FileMetadata[]> {
         const baseUrl = SERVER_API_URL + 'crops/' + this.context.cropName;
         const params: any = { observationId }
-        return this.http.get<FileMetadata[]>(baseUrl + '/filemetadata', {params, observe: 'body'});
+        return this.http.get<FileMetadata[]>(baseUrl + '/filemetadata', { params, observe: 'body' });
+    }
+
+    upload(file: File, observationUnitUUID, termId): Observable<FileMetadata> {
+        const formData: FormData = new FormData();
+        formData.append('file', file, file.name);
+        const headers = new Headers();
+        headers.append('Content-Type', '');
+
+        const options = {
+            params: {
+                observationUnitUUID,
+                termId
+            },
+        };
+
+        const baseUrl = SERVER_API_URL + 'crops/' + this.context.cropName;
+        return this.http.post<FileMetadata>(baseUrl + '/files', formData, options);
     }
 
 }
