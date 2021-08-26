f8d2f64fb1d324a3d957d77eed71935228540f86
Leonel Kovacic <leonel@leafnode.io>
2021-08-26 15:12:23 -0300

    IBP-4866: added 'sort label by' value when preset is saved

--- src/main/jhipster/src/main/webapp/app/label-printing/label-printing.component.ts
+++ src/main/jhipster/src/main/webapp/app/label-printing/label-printing.component.ts
@@ -149,6 +149,7 @@ export class LabelPrintingComponent implements OnInit {
 
             this.labelTypes = labelTypeList.map((x) => Object.assign({}, x));
             this.fieldsSelected = labelFieldsSelected;
+            this.sortBySelected = (presetSetting.sortBy) ? presetSetting.sortBy : '';
 
             setTimeout(() => {
                 $('#leftSelectedFields').empty();
@@ -380,6 +381,9 @@ export class LabelPrintingComponent implements OnInit {
         preset.barcodeSetting = barcodeSetting;
         preset.fileConfiguration = fileConfiguration;
         preset.includeHeadings = this.labelPrintingData.includeHeadings;
+        if (this.sortBySelected) {
+            preset.sortBy = this.sortBySelected;
+        }
 
         const presetSetting = this.presetSettings.filter((x) => x.name === preset.name)[0];
 
@@ -408,6 +412,7 @@ export class LabelPrintingComponent implements OnInit {
                     presetSetting.barcodeSetting = preset.barcodeSetting;
                     presetSetting.fileConfiguration = preset.fileConfiguration;
                     presetSetting.includeHeadings = preset.includeHeadings;
+                    presetSetting.sortBy = preset.sortBy;
                     this.alertService.success('label-printing.update.preset.success');
                 }, (response) => {
                     if (response.error.errors[0].message) {
