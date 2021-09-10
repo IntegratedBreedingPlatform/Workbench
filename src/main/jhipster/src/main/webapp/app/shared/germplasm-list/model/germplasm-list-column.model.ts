e6f48f2cfcab4cca93ad8f3d30de3ebd125ac0cb
Leonel Kovacic <leonel@leafnode.io>
2021-09-10 15:21:48 -0300

    IBP-4901: set selected property of GermplasmListColumn model as required

--- src/main/jhipster/src/main/webapp/app/shared/germplasm-list/model/germplasm-list-column.model.ts
+++ src/main/jhipster/src/main/webapp/app/shared/germplasm-list/model/germplasm-list-column.model.ts
@@ -2,9 +2,8 @@ export class GermplasmListColumn {
     constructor(public id: number,
                 public name: string,
                 public category: GermplasmListColumnCategory,
-                public typeId?: number,
-                // TODO: this value should be required once we are saved the customize view
-                public selected?: boolean) {
+                public selected: boolean,
+                public typeId?: number) {
     }
 
 }
