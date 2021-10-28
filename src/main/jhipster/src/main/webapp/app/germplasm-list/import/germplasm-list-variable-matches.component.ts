26924ce53c1728947ca31674b08a2696af08566a
cuenyad <cuenya.diego@gmail.com>
2021-10-28 13:33:28 -0300

    Refactor and clean up code.
    Issue: IBP-5088
    Review: none

--- src/main/jhipster/src/main/webapp/app/germplasm-list/import/germplasm-list-variable-matches.component.ts
+++ src/main/jhipster/src/main/webapp/app/germplasm-list/import/germplasm-list-variable-matches.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ParamContext } from '../../shared/service/param.context';
-import { PopupService } from '../../shared/modal/popup.service';
 import { AlertService } from '../../shared/alert/alert.service';
-import { GermplasmService } from '../../shared/germplasm/service/germplasm.service';
-import { GermplasmManagerContext } from '../../germplasm-manager/germplasm-manager.context';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { JhiEventManager } from 'ng-jhipster';
 import { GermplasmListImportContext } from './germplasm-list-import.context';
 import { ModalConfirmComponent } from '../../shared/modal/modal-confirm.component';
 import { GermplasmListImportUpdateComponent } from './germplasm-list-import-update.component';
+import { toUpper } from '../../shared/util/to-upper';
+import { GermplasmListService } from '../../shared/germplasm-list/service/germplasm-list.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { formatErrorList } from '../../shared/alert/format-error-list';
+import { ListComponent } from '../list.component';
 
 @Component({
     selector: 'jhi-germplasm-list-variable-matches.component',
@@ -27,7 +29,8 @@ export class GermplasmListVariableMatchesComponent implements OnInit {
 
     isLoading: boolean;
     isSaving: boolean;
-
+    rows = [];
+    variables: { [key: string]: any; } = {};
 
     constructor(
         private route: ActivatedRoute,
@@ -36,7 +39,7 @@ export class GermplasmListVariableMatchesComponent implements OnInit {
         private modalService: NgbModal,
         private paramContext: ParamContext,
         private alertService: AlertService,
-        private germplasmService: GermplasmService,
+        private germplasmListService: GermplasmListService,
         private eventManager: JhiEventManager,
         private context: GermplasmListImportContext
     ) {
@@ -44,6 +47,38 @@ export class GermplasmListVariableMatchesComponent implements OnInit {
 
     ngOnInit() {
         this.listId = Number(this.route.snapshot.queryParamMap.get('listId'));
+        this.rows = [];
+        this.context.newVariables.forEach((variable) => {
+            const variableName = variable.alias ? variable.alias : variable.name;
+            const entryDetail = {
+                id: variable.id, name: variableName,
+                description: variable.description,
+                exitsInlist: false
+            };
+            this.variables[toUpper(variableName)] = entryDetail;
+            this.rows.push(entryDetail);
+        });
+
+        this.context.variablesOfTheList.forEach((variable) => {
+            const variableName = variable.alias ? variable.alias : variable.name;
+            const entryDetail = {
+                id: variable.id,
+                name: variableName, description: variable.description,
+                exitsInlist: true
+            };
+            this.variables[toUpper(variableName)] = entryDetail;
+            this.rows.push(entryDetail);
+        });
+
+        this.context.unknownVariableNames.forEach((variableName) => {
+            const entryDetail = {
+                id: null,
+                name: variableName,
+                description: '',
+                exitsInlist: false
+            };
+            this.rows.push(entryDetail);
+        });
     }
 
     back() {
