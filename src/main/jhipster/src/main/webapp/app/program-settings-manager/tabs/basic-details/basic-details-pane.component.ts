3b7818d8a38f5ff6a870275b940f2fb9b7a66dbf
cuenyad <cuenya.diego@gmail.com>
2021-12-29 09:02:45 -0300

    Adding Basic details component definitions.
    Issue: IBP-5342
    Review: none

--- src/main/jhipster/src/main/webapp/app/program-settings-manager/tabs/basic-details/basic-details-pane.component.ts
+++ src/main/jhipster/src/main/webapp/app/program-settings-manager/tabs/basic-details/basic-details-pane.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Program } from '../../../shared/program/model/program';
 
 @Component({
     selector: 'jhi-basic-details-pane',
-    templateUrl: 'basic-details-pane.component.html'
+    templateUrl: 'basic-details-pane.component.html',
+    styleUrls: ['basic-details-panel.component.css']
 })
-export class BasicDetailsPaneComponent {
+export class BasicDetailsPaneComponent implements OnInit, OnDestroy {
 
+    isLoading: boolean;
+    program: Program = new Program();
+    programOrg: Program;
+    startDate: NgbDate;
+
+    constructor() {
+        this.program = {};
+    }
+
+    ngOnDestroy(): void {
+    }
+
+    ngOnInit(): void {
+    }
+
+    delete() {
+    }
+
+    save() {
+
+    }
+
+    isFormValid(f) {
+        return f.form.valid && !this.isLoading && this.program.name && this.program.crop
+            && this.startDate;
+    }
+
+    reset() {
+    }
+
+    private onError(response: HttpErrorResponse) {
+    }
 }
