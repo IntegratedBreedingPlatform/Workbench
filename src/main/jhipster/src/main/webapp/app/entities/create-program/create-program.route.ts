a5ab523b832bdbac77282764e30d1bc816d63fed
leonel@leafnode.io <leonelkovacic@gmail.com>
2021-12-30 14:07:05 -0300

    IBP-4838: fixed compilation warning

--- src/main/jhipster/src/main/webapp/app/entities/create-program/create-program.route.ts
+++ src/main/jhipster/src/main/webapp/app/entities/create-program/create-program.route.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { CreateProgramPopupComponent } from './create-program-dialog.component';
 
-
 export const CREATE_PRORGAM_ROUTER: Routes =  [
     {
         path: 'create-program',
