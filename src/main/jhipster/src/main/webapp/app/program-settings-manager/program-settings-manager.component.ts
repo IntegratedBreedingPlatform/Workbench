8fd42df47480e0c895130314e45406bcba5be2ce
cuenyad <cuenya.diego@gmail.com>
2021-12-30 14:52:14 -0300

    Implement Help link to redirect manage program settings wiki.
    Issue: IBP-5342
    Review: none

--- src/main/jhipster/src/main/webapp/app/program-settings-manager/program-settings-manager.component.ts
+++ src/main/jhipster/src/main/webapp/app/program-settings-manager/program-settings-manager.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { ParamContext } from '../shared/service/param.context';
 import { JhiLanguageService } from 'ng-jhipster';
+import { HELP_MANAGE_PROGRAM_SETTINGS } from '../app.constants';
+import { HelpService } from '../shared/service/help.service';
 
 @Component({
     selector: 'jhi-program-settings-manager',
     templateUrl: './program-settings-manager.component.html'
 })
 export class ProgramSettingsManagerComponent {
+
+    helpLink: string;
+
     constructor(
         private jhiLanguageService: JhiLanguageService,
+        private helpService: HelpService,
         private paramContext: ParamContext
     ) {
         this.paramContext.readParams();
+        if (!this.helpLink || !this.helpLink.length) {
+            this.helpService.getHelpLink(HELP_MANAGE_PROGRAM_SETTINGS).toPromise().then((response) => {
+                if (response.body) {
+                    this.helpLink = response.body;
+                }
+            }).catch((error) => {
+            });
+        }
     }
 }
