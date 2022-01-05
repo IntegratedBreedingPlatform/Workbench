d73cc538ace35141f379f30e3ae34aaedc0d63e4
cuenyad <cuenya.diego@gmail.com>
2022-01-05 18:15:39 -0300

    Refactoring route.
    Issue: IBP-5335
    Review: none

--- src/main/jhipster/src/main/webapp/app/crop-settings-manager/crop-settings-manager.route.ts
+++ src/main/jhipster/src/main/webapp/app/crop-settings-manager/crop-settings-manager.route.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CropSettingsManagerComponent } from './crop-settings-manager.component';
 import { RouteAccessService } from '../shared';
 import { MANAGE_CROP_SETTINGS_PERMISSIONS } from '../shared/auth/permissions';
-import { nameTypePopupRoutes, nameTypeRoutes } from './name-type/name-type-route';
+import { LocationsPaneComponent } from './locations/locations-pane.component';
+import { NameTypesPaneComponent } from './name-types/name-types-pane.component';
+import { NameTypesResolvePagingParams } from './name-types/name-types-resolve-paging-params';
+import { NameTypeEditPopupComponent } from './name-types/name-type-edit-dialog.component';
 
 export const CROP_SETTINGS_MANAGER_ROUTES: Routes = [
-    ...nameTypePopupRoutes,
     {
         path: 'crop-settings-manager',
         component: CropSettingsManagerComponent,
@@ -14,8 +16,29 @@ export const CROP_SETTINGS_MANAGER_ROUTES: Routes = [
             authorities: [...MANAGE_CROP_SETTINGS_PERMISSIONS]
         },
         canActivate: [RouteAccessService],
-        children: [...nameTypeRoutes,
+        children: [
+            {
+                path: '',
+                redirectTo: 'locations',
+                pathMatch: 'full'
+            },
+            {
+                path: 'locations',
+                component: LocationsPaneComponent
+            },
+            {
+                path: 'name-types',
+                component: NameTypesPaneComponent,
+                resolve: {
+                    'pagingParams': NameTypesResolvePagingParams
+                }
+            }
         ],
+    },
+    {
+        path: 'name-type-edit-dialog',
+        component: NameTypeEditPopupComponent,
+        outlet: 'popup'
     }
 
 ];
