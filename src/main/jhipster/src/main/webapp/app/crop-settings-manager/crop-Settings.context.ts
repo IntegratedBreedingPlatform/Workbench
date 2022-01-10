a5ed727203a9eaa3dba213087fa67b6f35b4f9bf
cuenyad <cuenya.diego@gmail.com>
2022-01-10 08:50:43 -0300

    Adding breeding method.
    Issue: IBP-5336
    Review: none

--- src/main/jhipster/src/main/webapp/app/crop-settings-manager/crop-Settings.context.ts
+++ src/main/jhipster/src/main/webapp/app/crop-settings-manager/crop-Settings.context.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { NameTypeDetails } from '../shared/germplasm/model/name-type.model';
 import { Location } from '../shared/location/model/location';
+import { BreedingMethod } from '../shared/breeding-method/model/breeding-method';
 
 @Injectable()
 export class CropSettingsContext {
     nameTypeDetails: NameTypeDetails;
+    breedingMethod: BreedingMethod;
     location: Location;
 }
