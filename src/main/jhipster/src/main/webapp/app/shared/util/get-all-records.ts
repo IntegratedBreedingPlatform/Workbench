ace025713f896bf5500f4658a050ce72efd7bd41
Nahuel Soldevilla <nahuel@leafnode.io>
2022-01-13 10:33:38 -0300

    IBP-5375: prototype to get all records with x-total-count

--- src/main/jhipster/src/main/webapp/app/shared/util/get-all-records.ts
+++ src/main/jhipster/src/main/webapp/app/shared/util/get-all-records.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
-import { expand, reduce } from 'rxjs/operators';
+import { expand, map, reduce } from 'rxjs/operators';
+import { HttpResponse } from '@angular/common/http';
 
 const GET_ALL_PAGE_SIZE = 1000;
 const GET_ALL_LIMIT = 50000;
@@ -8,11 +9,17 @@ const GET_ALL_LIMIT = 50000;
  * Consume a paginated api until no more records are found
  * @param f function that does the http request
  */
-export function getAllRecords<T>(f: (page: number, pageSize: number) => Observable<T[]>): Observable<T[]> {
+export function getAllRecords<T>(f: (page: number, pageSize: number) => Observable<HttpResponse<T[]>>): Observable<T[]> {
     let page = 0;
+    let totalCount;
     return f(page, GET_ALL_PAGE_SIZE).pipe(
+        map((resp: HttpResponse<T[]>) => {
+            totalCount = resp['X-Total-Count'];
+            return resp.body;
+        }),
         expand((resp) => {
-            if (resp.length && ((page + 1) * GET_ALL_PAGE_SIZE) <= GET_ALL_LIMIT) {
+            const nextPageRecord = (page + 1) * GET_ALL_PAGE_SIZE;
+            if (resp.length && nextPageRecord <= GET_ALL_LIMIT && nextPageRecord <= totalCount) {
                 page++;
                 return f(page, GET_ALL_PAGE_SIZE);
             } else {
