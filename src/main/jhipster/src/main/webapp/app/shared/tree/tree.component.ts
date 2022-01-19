07974c8bc3b89e8eb526443bb0278139625eb87c
Clarysabel Tovar <clarysabel@leafnode.io>
2022-01-19 18:45:40 -0300

    Fully allowed program folders for germplasm lists IBP-5285

--- src/main/jhipster/src/main/webapp/app/shared/tree/tree.component.ts
+++ src/main/jhipster/src/main/webapp/app/shared/tree/tree.component.ts
@@ -101,10 +101,6 @@ export class TreeComponent implements OnInit {
                 this.alertService.error('bmsjHipsterApp.tree-table.messages.folder.cannot.move.has.children', { folder: this.draggedNode.data.name });
                 this.draggedNode = null;
                 return;
-            } else if (node.data.id === 'CROPLISTS' && !this.draggedNode.leaf) {
-                this.alertService.error('bmsjHipsterApp.tree-table.messages.folder.move.to.crop.list.not.allowed');
-                this.draggedNode = null;
-                return;
             } else if (node.leaf) {
                 this.alertService.error('bmsjHipsterApp.tree-table.messages.folder.move.not.allowed');
                 this.draggedNode = null;
