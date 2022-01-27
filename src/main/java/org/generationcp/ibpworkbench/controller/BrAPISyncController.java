93fc2078b362ed4a946694305253e55481a509ce
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2022-01-27 15:55:16 +0800

    Initial Commit
    IBP-5398

---
+++ src/main/java/org/generationcp/ibpworkbench/controller/BrAPISyncController.java
@@ -0,0 +1,17 @@
+package org.generationcp.ibpworkbench.controller;
+
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/brapi-sync")
+public class BrAPISyncController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) throws MiddlewareQueryException {
+		return "brapi-sync/index";
+	}
+}
