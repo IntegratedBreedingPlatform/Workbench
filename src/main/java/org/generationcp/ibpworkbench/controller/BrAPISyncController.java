435f7605e5c132c6a6afd29983c627cb213fafe3
mderamos <marc.ramos@leafnode.io>
2022-02-08 09:29:46 +0800

    Fixed paths
    IBP-5398

--- src/main/java/org/generationcp/ibpworkbench/controller/BrAPISyncController.java
+++ src/main/java/org/generationcp/ibpworkbench/controller/BrAPISyncController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/brapi-sync")
+@RequestMapping("/pages/brapi-sync/")
 public class BrAPISyncController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(Model model) throws MiddlewareQueryException {
-		return "/brapi-sync/index";
+		return "brapi-sync/index";
 	}
 }
