d03bfcf50a77664530f18c5939f7f64f1288137f
Aldrin Batac <aldrin.batac@leafnode.io>
2022-03-21 12:46:30 +0800

    IBP-5531 IBP-5550 Adjust MSA to read the summary statistics data from the new SUMMARY STATISTICS dataset.

--- src/main/java/org/generationcp/ibpworkbench/actions/RunMultiSiteAction.java
+++ src/main/java/org/generationcp/ibpworkbench/actions/RunMultiSiteAction.java
@@ -208,7 +208,7 @@ public class RunMultiSiteAction implements ClickListener {
 	}
 
 	DataSet getSummaryStatsDataSet(final int studyId) {
-		return this.studyDataManager.findOneDataSetByType(studyId, DatasetTypeEnum.SUMMARY_DATA.getId());
+		return this.studyDataManager.findOneDataSetByType(studyId, DatasetTypeEnum.SUMMARY_STATISTICS.getId());
 	}
 
 	String generateInputFileName(final Project project) {
