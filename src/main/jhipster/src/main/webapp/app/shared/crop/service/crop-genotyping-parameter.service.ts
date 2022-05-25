ebae8d32cfcd4f6291b337a6a0a1227875bfea54
Aldrin Batac <aldrin.batac@leafnode.io>
2022-05-25 08:07:28 +0800

    IBP-5652 Use plural crop-genotyping-parameters

--- src/main/jhipster/src/main/webapp/app/shared/crop/service/crop-genotyping-parameter.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/crop/service/crop-genotyping-parameter.service.ts
@@ -10,11 +10,11 @@ export class CropGenotypingParameterService {
     }
 
     getByCropName(cropName: string): Observable<CropGenotypingParameter> {
-        return this.http.get<CropGenotypingParameter>(SERVER_API_URL + `crops/${cropName}/crop-genotyping-parameter`);
+        return this.http.get<CropGenotypingParameter>(SERVER_API_URL + `crops/${cropName}/crop-genotyping-parameters`);
     }
 
     getToken(cropName: string): Observable<string> {
         const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-        return this.http.get<string>(SERVER_API_URL + `crops/${cropName}/crop-genotyping-parameter/token`, { headers, responseType: 'text' as 'json' });
+        return this.http.get<string>(SERVER_API_URL + `crops/${cropName}/crop-genotyping-parameters/token`, { headers, responseType: 'text' as 'json' });
     }
 }
