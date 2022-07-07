983d27fa86a3a4d144e255023a567d1add6fcbaf
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-07-07 18:02:07 -0300

    IBP-5722: append brapi-sync base url as source

--- src/main/jhipster/src/main/webapp/app/shared/crop-parameter/service/crop-parameter.service.ts
+++ src/main/jhipster/src/main/webapp/app/shared/crop-parameter/service/crop-parameter.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { ParamContext } from '../../service/param.context';
 import { SERVER_API_URL } from '../../../app.constants';
 import { CropParameter } from '../model/crop-parameter';
+import { CropParameterTypeEnum } from '../model/crop-parameter-type-enum';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class CropParameterService {
@@ -12,13 +14,19 @@ export class CropParameterService {
     ) {
     }
 
+    getCropParameter(cropName: string, programUUID: string, key: CropParameterTypeEnum): Observable<CropParameter> {
+        const baseUrl = SERVER_API_URL + 'crops/' + cropName;
+        return this.http.get<CropParameter>(baseUrl + `/crop-parameters/${key}?programUUID=${programUUID}`);
+    }
+
     getCropParameters() {
         const baseUrl = SERVER_API_URL + 'crops/' + this.context.cropName;
-        return this.http.get<CropParameter[]>(baseUrl + '/crop-parameters?programUUID=' + this.context.programUUID);
+        return this.http.get<CropParameter[]>(baseUrl + `/crop-parameters?programUUID=${this.context.programUUID}`);
     }
 
     modifyCropParameters(key, value) {
         const baseUrl = SERVER_API_URL + 'crops/' + this.context.cropName;
-        return this.http.patch<CropParameter[]>(baseUrl + `/crop-parameters/${key}?programUUID=` + this.context.programUUID, {value});
+        return this.http.patch<CropParameter[]>(baseUrl + `/crop-parameters/${key}?programUUID=${this.context.programUUID}`, {value});
     }
+
 }
