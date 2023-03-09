c3bd5782333db5e8bf8ef613178ff6db76484a4f
Aldrin Batac <aldrin.batac@leafnode.io>
2023-03-09 12:51:14 +0800

    IBP-6188 / IBP-6236 - Bug fix.

--- src/main/jhipster/src/main/webapp/app/entities/sample/genotype/genotype.modal.component.ts
+++ src/main/jhipster/src/main/webapp/app/entities/sample/genotype/genotype.modal.component.ts
@@ -149,15 +149,15 @@ export class GenotypeModalComponent implements OnInit {
                 if (samples && samples.length) {
                     // Get the Callsets (Individuals) that are associated to the specified genotype samples.
                     return this.retrieveCallsets(samples);
+                } else {
+                    this.alertService.error('genotyping.no.genotyping.samples.found');
+                    this.isVariantsLoading = false;
                 }
                 return Observable.empty();
             })).subscribe((callSets) => {
                 if (callSets && callSets.length) {
                     // Get the variants (Markers) associated to the specified Callsets (Individuals)
                     this.loadVariants(this.selectedVariantSet.variantSetDbId, callSets);
-                } else {
-                    this.alertService.error('genotyping.no.genotyping.samples.found');
-                    this.isVariantsLoading = false;
                 }
             });
 
